807f3dd035d62e3437fb13ea5105a4fde477fbee
vitalii_musikhin <bloodlokiss14@gmail.com>
2023-01-20 23:48:22 +0300

    add Solution javaSyntax task 0101

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task01/task0101/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.pro.task01.task0101;
+
+/* 
+Первая программа
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+    }
+}
