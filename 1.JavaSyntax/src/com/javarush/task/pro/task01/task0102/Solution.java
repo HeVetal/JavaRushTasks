5ee7baef6de7e14ae20f6ba5639005d3b25f3d4d
vitalii_musikhin <bloodlokiss14@gmail.com>
2023-01-21 00:03:42 +0300

    add Solution javaSyntax task 0102

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task01/task0102/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.pro.task01.task0102;
+
+/* 
+Включить аварийное питание и ручное управление
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+    }
+}
