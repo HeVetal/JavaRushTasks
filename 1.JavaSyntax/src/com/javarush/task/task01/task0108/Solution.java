64c315b9f6173c336642da72c27d5bcd86ffc9db
vitalii_musikhin <bloodlokiss14@gmail.com>
2023-01-22 21:25:09 +0300

    add Solution javaSyntax task 0108 test2

--- 1.JavaSyntax/src/com/javarush/task/task01/task0108/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0108/Solution.java
@@ -8,7 +8,7 @@ public class Solution {
     public static void main(String[] args) {
         int a = 3;
         int b = 2;
-        int x;
+        int x = 2;
         //System.out.print("два");
         System.out.print(b);
         System.out.print(" плюс ");
