809a02eab4cb3fa8e2e4c2b8679163684cfa4890
vitalii_musikhin <bloodlokiss14@gmail.com>
2023-01-22 20:23:06 +0300

    add Solution javaSyntax task 0103

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0101/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.task01.task0101;
+
+/* 
+Я думаю, быть программистом круто
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        System.out.println("Я думаю, быть программистом круто");
+    }
+}
