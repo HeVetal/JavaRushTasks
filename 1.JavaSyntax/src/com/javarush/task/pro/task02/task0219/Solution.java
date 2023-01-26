2b4dbdb8e2bd0af6dcaa2c4fd9c59a4cd7602526
HeVetal <heavyve@gmail.com>
2023-01-26 21:37:58 +0300

    add Solution javaSyntax task 0219

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0219/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.pro.task02.task0219;
+
+/* 
+Баги робокопа
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        int id = 297774;
+        int age = 30;
+        String documents = "ID";
+        String behavior ="normal";
+    }
+}
