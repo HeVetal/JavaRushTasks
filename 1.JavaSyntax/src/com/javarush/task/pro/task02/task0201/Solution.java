ad47195ba18f383bc87fd4f0890e6e4d7a9ec025
HeVetal <heavyve@gmail.com>
2023-01-26 21:41:06 +0300

    add Solution javaSyntax task 0201

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0201/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.pro.task02.task0201;
+
+/* 
+«Я по объявлению…»
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        int intValue = 2, numericValue = 3, size = 4, number = 5;
+
+    }
+}
