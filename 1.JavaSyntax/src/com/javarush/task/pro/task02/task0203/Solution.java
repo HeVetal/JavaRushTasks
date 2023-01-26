d2a93c41cde5c040d5a866b4c65136fd254cd154
HeVetal <heavyve@gmail.com>
2023-01-26 22:30:53 +0300

    add Solution javaSyntax task 0203t

--- 1.JavaSyntax/src/com/javarush/task/pro/task02/task0203/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0203/Solution.java
@@ -9,8 +9,8 @@ public class Solution {
     public static void main(String[] args) {
         int number = 546;
 
-        int lastDigit = number % 10; //напишите тут ваш код
+        int lastDigit; //напишите тут ваш код
 
-        System.out.println(lastDigit);
+        //System.out.println(lastDigit);
     }
 }
