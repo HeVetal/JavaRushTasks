b9671cbfc5f589fd93553982771fc0579d951132
HeVetal <heavyve@gmail.com>
2023-01-27 19:27:50 +0300

    add Solution javaSyntax task 0206

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0206/Solution.java
@@ -0,0 +1,9 @@
+package com.javarush.task.pro.task02.task0206;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        String word = "Слово", phrase = "два слова", line = "линия", text = "текст";
+    }
+}
