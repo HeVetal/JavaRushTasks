326d0da1c60a9de12ee9c0c7a96ef03a701defbd
HeVetal <heavyve@gmail.com>
2023-01-27 19:23:10 +0300

    add Solution javaSyntax task 0216

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0216/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.pro.task02.task0216;
+
+/* 
+Генератор позитивных новостей
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        String name = "Vasa", date = "01.02.30", time = "20.10", noun = "2000", adjective = "WunderWafen", verb = "передает";
+        System.out.println("Новости:");
+        System.out.println(name + " " + date + " " + time + " изобрел " + adjective + " " + noun + " который " + verb + " террабайты данных в минуту.");
+    }
+}
