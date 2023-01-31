33239599bfdfb62044f474e2d8b362950fae3f0f
HeVetal <heavyve@gmail.com>
2023-01-31 23:00:03 +0300

    add task 0401

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0401/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.pro.task04.task0401;
+
+/* 
+Я никогда не буду работать за копейки
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String quote = "Я никогда не буду работать за копейки. Амиго";
+        //напишите тут ваш код
+        int count = 1;
+        while (count <= 100){
+            System.out.println(quote);
+
+            count++;
+        }
+
+    }
+}
