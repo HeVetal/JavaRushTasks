12c6be36e3a7c0ea3c11244a13fa8e9f167c6315
HeVetal <heavyve@gmail.com>
2023-02-02 19:37:11 +0300

    add task javarush 0402

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0402/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.pro.task04.task0402;
+
+import java.util.Scanner;
+
+/* 
+Все любят Мамбу
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String text = " любит меня.";
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        int count = 0;
+        while (count < 10){
+            System.out.println(name + text);
+            count++;
+
+        }
+    }
+}
