cfdf599ef5baa6628e8c8d0a7be5a7d853092972
HeVetal <heavyve@gmail.com>
2023-02-03 20:56:24 +0300

    add task 0421

--- 1.JavaSyntax/src/com/javarush/task/pro/task04/task0421/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0421/Solution.java
@@ -6,8 +6,15 @@ package com.javarush.task.pro.task04.task0421;
 
 public class Solution {
     public static void main(String[] args) {
-        //напишите тут ваш код
+        int count = 1;
+        int sum = 0;
+        while (count <= 100) {
+            if (count % 2 == 0) {
+                sum += count;
+            }
+            count++;
 
-
+        }
+        System.out.println(sum);
     }
 }
