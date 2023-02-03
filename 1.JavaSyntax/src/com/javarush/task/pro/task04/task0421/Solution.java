8900d0c3add423ca5f5658bb3da76c587b2bfeb3
HeVetal <heavyve@gmail.com>
2023-02-03 20:46:11 +0300

    add all

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0421/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.pro.task04.task0421;
+
+/* 
+Охлаждение ядра планеты
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+
+
+    }
+}
