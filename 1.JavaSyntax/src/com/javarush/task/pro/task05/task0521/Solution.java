a9ea6856f60b0d934dd5abba7b0b9c53d5b5b944
HeVetal <heavyve@gmail.com>
2023-02-07 20:40:56 +0300

    add solution JavaSyntax task 0521

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0521/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0521/Solution.java
@@ -6,7 +6,7 @@ package com.javarush.task.pro.task05.task0521;
 
 public class Solution {
 
-    public static int result;
+    public static int result = 45967;
 
     public static void main(String[] args) {
         String loop = "    for (double fahrenheit = -459.67; fahrenheit < 451; fahrenheit += 40) {\n";
