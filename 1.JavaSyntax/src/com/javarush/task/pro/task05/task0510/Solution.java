b5e37decad5f0fe6b7cff6492ab84ae5437072f7
HeVetal <heavyve@gmail.com>
2023-02-13 23:23:46 +0300

    add solution JavaSyntax task 0510 not finish

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0510/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.pro.task05.task0510;
+
+/* 
+Треугольный массив
+*/
+
+public class Solution {
+
+    public static int[][] result = new int[10][];
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+    }
+}
