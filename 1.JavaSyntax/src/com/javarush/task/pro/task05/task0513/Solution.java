2f24dc1dc113ab85cbca89f9b655f260a348edee
HeVetal <heavyve@gmail.com>
2023-02-15 23:37:44 +0300

    add solution JavaSyntax task 0513 not finish

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0513/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.pro.task05.task0513;
+
+import java.util.Arrays;
+
+/* 
+Выводим массивы
+*/
+
+public class Solution {
+
+    public static String[] strings = new String[]{"I", "love", "Java"};
+    public static int[] ints = new int[]{1, 2, 3, 4, 5, 6, 7, 8};
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+    }
+}
