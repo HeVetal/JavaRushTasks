328b8733938480cb5c356afde89a2c96a9369b0f
HeVetal <heavyve@gmail.com>
2023-02-18 22:59:42 +0300

    add solution JavaSyntax task 0618 not finish

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0618/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.pro.task06.task0618;
+
+/* 
+Злобный призрак
+*/
+
+public class Solution {
+    public static String material= "материальный";
+    public static String immaterial= "нематериальный";
+
+    public static void main(String[] args) {
+        System.out.println("Из болота вылез нематериальный монстр...");
+
+        //напишите тут ваш код
+    }
+
+    //напишите тут ваш код
+}
