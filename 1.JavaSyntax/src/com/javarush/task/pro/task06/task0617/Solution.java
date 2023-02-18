4fb186408a08e30a6946029312e6adb91bb7a023
HeVetal <heavyve@gmail.com>
2023-02-18 22:58:34 +0300

    add solution JavaSyntax task 0617 not finish

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0617/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.pro.task06.task0617;
+
+/* 
+Зачарованный меч
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("Из болота лезут упыри и скелеты...");
+
+        System.out.println("Амиго активирует меч и легко уничтожает скелетов и упырей.");
+    }
+
+    //напишите тут ваш код
+}
