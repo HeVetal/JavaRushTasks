c9b5db24f82d7ae49b88bdf657e1eda642f3089a
HeVetal <heavyve@gmail.com>
2023-02-18 23:01:17 +0300

    add solution JavaSyntax task 0606 not finish

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0606/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.pro.task06.task0606;
+
+/* 
+Универсальный солдат
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void universalMethod(){
+
+    }
+
+    //напишите тут ваш код
+}
