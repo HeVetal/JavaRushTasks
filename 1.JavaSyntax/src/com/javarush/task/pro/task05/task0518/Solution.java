bbd34774bdc1e4da3b4510a4f25f35a7c938e537
Vetal <bloodlokiss14@gmail.com>
2023-02-18 20:22:03 +0100

    x,y

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0518/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0518/Solution.java
@@ -14,6 +14,6 @@ public class Solution {
         //напишите тут ваш код
         Arrays.sort(array);
         System.out.println(Arrays.toString(array));
-        int x;
+        int x,y;
     }
 }
