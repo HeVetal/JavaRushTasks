a2ad4cab6ea3f99e019f0cd48497e45dd76a4e20
HeVetal <heavyve@gmail.com>
2023-02-18 21:09:59 +0300

    add solution JavaSyntax task 0513

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0513/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0513/Solution.java
@@ -13,5 +13,7 @@ public class Solution {
 
     public static void main(String[] args) {
         //напишите тут ваш код
+        System.out.println(Arrays.toString(strings));
+        System.out.println(Arrays.toString(ints));
     }
 }
---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0516/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.pro.task05.task0516;
+
+import java.util.Arrays;
+
+/* 
+Заполняем массив
+*/
+
+public class Solution {
+
+    public static int[] array = new int[20];
+    public static int valueStart = 10;
+    public static int valueEnd = 13;
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        System.out.println(Arrays.toString(array));
+    }
+}
