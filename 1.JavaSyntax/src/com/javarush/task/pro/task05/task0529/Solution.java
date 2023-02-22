43e3d4f8df71e06aeda2f7faf72e2bbf39d6b7a1
HeVetal <heavyve@gmail.com>
2023-02-22 23:30:24 +0300

    solution JavaSyntax task 0529

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0529/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0529/Solution.java
@@ -1,8 +1,10 @@
+
+
 package com.javarush.task.pro.task05.task0529;
 
 import java.util.Arrays;
 
-/* 
+/*
 Галаксианские роботанки (1)
 */
 
@@ -12,9 +14,21 @@ public class Solution {
     public static String hit = "🎯";
     public static int width = 30;
     public static int height = 10;
-    public static String[][] field = new String[width][height];
+    public static String[][] field = new String[height][width];
 
     public static void main(String[] args) {
         //напишите тут ваш код
+
+        for (int i = 0; i < field.length; i++) {
+            Arrays.fill(field[i], empty);
+        }
+
+
+        for (int i = 0; i < field.length; i++) {
+            for (int j = 0; j < field[i].length; j++) {
+                System.out.print(field[i][j]);
+            }
+            System.out.println();
+        }
     }
 }
