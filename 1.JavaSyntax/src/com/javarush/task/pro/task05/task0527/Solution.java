03bea9b9f1fa2b5802f58ca59e1d38023152bb7c
HeVetal <heavyve@gmail.com>
2023-03-02 21:43:12 +0300

    add solution JavaSyntax task 0527

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0527/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0527/Solution.java
@@ -9,6 +9,16 @@ public class Solution {
 
     public static void main(String[] args) {
         //напишите тут ваш код
+        for (int i = 0; i < array.length; i++) {
+            array[i] = new int[2 * i + 1];
+            }
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                array[i][j] = i + j;
+            }
+
+        }
 
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array[i].length; j++) {
