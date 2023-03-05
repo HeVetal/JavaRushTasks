59b231201b4efa812cf39022260d3a19679158d3
HeVetal <heavyve@gmail.com>
2023-03-05 21:30:25 +0300

    add solution JavaSyntax task

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0608/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.pro.task06.task0608;
+
+/* 
+Кубический калькулятор
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    //напишите тут ваш код
+}
