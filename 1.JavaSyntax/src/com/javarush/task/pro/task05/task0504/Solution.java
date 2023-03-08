5fefdfa06a2723ee1a3cddbd4246a7cd0fff5d45
HeVetal <heavyve@gmail.com>
2023-03-08 21:51:54 +0300

    add solution JavaSyntax task 0516

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0504/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0504/Solution.java
@@ -4,13 +4,15 @@ package com.javarush.task.pro.task05.task0504;
 Объединяем массивы
 */
 
+import java.util.Arrays;
+
 public class Solution {
     public static int[] firstArray = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
     public static int[] secondArray = new int[]{10, 11, 12, 13, 14, 15, 16, 17, 18, 19};
     public static int[] resultArray;
 
     public static void main(String[] args) {
-        resultArray = new int[firstArray.length + secondArray.length];
+      //  resultArray = new int[firstArray.length + secondArray.length];
 //        for( int i = 0; i < firstArray.length; i++){
 //            resultArray[i] = firstArray[i];
 //        }
@@ -18,14 +20,17 @@ public class Solution {
 //            resultArray[i + firstArray.length] = secondArray[i];
 //        }
 
-        for (int i = 0; i < resultArray.length; i++) {
-            if(i < firstArray.length){
-                resultArray[i] = firstArray[i];
-            }else {
-                resultArray[i] = secondArray[i - firstArray.length];
-            }
+//        for (int i = 0; i < resultArray.length; i++) {
+//            if(i < firstArray.length){
+//                resultArray[i] = firstArray[i];
+//            }else {
+//                resultArray[i] = secondArray[i - firstArray.length];
+//            }
+//
+//        }
+        resultArray = Arrays.copyOf(firstArray,firstArray.length + secondArray.length);
+        System.arraycopy(secondArray,0,resultArray,firstArray.length,secondArray.length);
 
-        }
         for (int i = 0; i < resultArray.length; i++) {
             System.out.print(resultArray[i] + ", ");
         }
