e340a2e365923c1d2d3fbdc68339d3e1d39ea264
HeVetal <heavyve@gmail.com>
2023-03-10 20:30:08 +0300

    add solution JavaSyntax task 0510

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0510/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0510/Solution.java
@@ -9,6 +9,33 @@ public class Solution {
     public static int[][] result = new int[10][];
 
     public static void main(String[] args) {
+        for (int i = 0,k = 1; i < result.length; i++,k++) {
+
+            result[i] = new int[k];
+
+        }
+
+        for (int i = 0; i < result.length; i++) {
+            for (int j = 0; j < result[i].length; j++) {
+                result[i][j] = i + j ;
+            }
+        }
+
+        for (int i = 0; i < result.length; i++) {
+            for (int j = 0; j < result[i].length; j++) {
+                System.out.print(result[i][j] + " ");
+            }
+            System.out.println();
+        }
+//        правиьный вариант
+//        for (int i = 0; i < result.length; i++) {
+//            result[i] = new int[i + 1];
+//            for (int j = 0; j < result[i].length; j++) {
+//                result[i][j] = i + j;
+//                System.out.print(result[i][j] + " ");
+//            }
+//            System.out.println();
+//        }
         //напишите тут ваш код
     }
 }
