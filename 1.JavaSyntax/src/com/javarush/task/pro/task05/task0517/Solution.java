8bcd7b213238a33fc53585ece109f57532ec1793
HeVetal <heavyve@gmail.com>
2023-03-12 00:01:03 +0300

    add solution JavaSyntax task 0517

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0517/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0517/Solution.java
@@ -12,7 +12,16 @@ public class Solution {
     public static int[] array = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
     public static void main(String[] args) {
-        //напишите тут ваш код
+        int firstPart = 0;
+        if(array.length % 2 == 0) {
+            firstPart = array.length/2;
+        } else {
+            firstPart = array.length/2 + 1;
+        }
+
+        result[0] = Arrays.copyOfRange(array, 0, firstPart);
+
+        result[1] = Arrays.copyOfRange(array, firstPart, array.length);
         System.out.println(Arrays.deepToString(result));
     }
 }
