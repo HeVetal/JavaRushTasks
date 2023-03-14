79f567c44a9bae9ef80a500def44b6096c9ffced
HeVetal <heavyve@gmail.com>
2023-03-14 20:58:58 +0300

    add solution JavaSyntax task 0604

--- 1.JavaSyntax/src/com/javarush/task/pro/task06/task0604/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0604/Solution.java
@@ -13,11 +13,11 @@ public class Solution {
         createCrew(navigator, pilot, secondPilot, flightEngineer);
     }
 
-    public static void createCrew(String name1, String name2, String name3, String name4){
+    public static void createCrew(String navigator, String pilot, String secondPilot, String flightEngineer){
         System.out.println("Четвёрка, которой предстоит покорить космос:");
-        System.out.println("Штурман: " + name1);
-        System.out.println("Пилот: " + name2);
-        System.out.println("Второй пилот: " + name3);
-        System.out.println("Бортинженер: " + name4);
+        System.out.println("Штурман: " + navigator);
+        System.out.println("Пилот: " + pilot);
+        System.out.println("Второй пилот: " + secondPilot);
+        System.out.println("Бортинженер: " + flightEngineer);
     }
 }
