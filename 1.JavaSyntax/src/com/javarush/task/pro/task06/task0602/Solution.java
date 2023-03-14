a7f4b851a88278996763a7de0decc06eef534357
HeVetal <heavyve@gmail.com>
2023-03-14 20:49:13 +0300

    add solution JavaSyntax task 0602

--- 1.JavaSyntax/src/com/javarush/task/pro/task06/task0602/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0602/Solution.java
@@ -7,13 +7,9 @@ package com.javarush.task.pro.task06.task0602;
 public class Solution {
     public static void main(String[] args) {
         //напишите тут ваш код
-        print1();
-        print2();
-    }
-    public static void print1(){
         System.out.println("Я — поэт, зовусь я Цветик.");
-    }
-    public static void print2(){
         System.out.println("От меня вам всем приветик.");
     }
+
+
 }
