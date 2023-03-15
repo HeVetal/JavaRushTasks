d0475285148f514fbd49677371eea9dba0e8bfc7
HeVetal <heavyve@gmail.com>
2023-03-15 20:01:42 +0300

    add solution JavaSyntax task 0615

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0609/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.pro.task06.task0609;
+
+/* 
+Кубический калькулятор в кубе
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static long cube(long a){
+        return a*a*a;
+    }
+
+    public static long ninthDegree(long b){
+
+        return cube(b) * cube(b) * cube(b) ;
+    }
+}
