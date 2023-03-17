291cd91c167fd50eadd5d0d80b389e70a37fec76
HeVetal <heavyve@gmail.com>
2023-03-17 19:08:39 +0300

    add solution JavaSyntax task 0703

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0703/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.pro.task07.task0703;
+
+/* 
+Минимальное целое
+*/
+
+public class Solution {
+    //напишите тут ваш код
+    int x = Integer.MIN_VALUE;
+    byte c = Byte.MIN_VALUE;
+    short v = Short.MIN_VALUE;
+    long b = Long.MIN_VALUE;
+}
