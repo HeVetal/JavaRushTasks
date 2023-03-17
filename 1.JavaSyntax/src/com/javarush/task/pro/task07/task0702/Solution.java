1b125c7e4a63be1c75165f77c20fdba66d2f888c
HeVetal <heavyve@gmail.com>
2023-03-17 19:06:48 +0300

    add solution JavaSyntax task 0702

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0702/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.pro.task07.task0702;
+
+/* 
+Максимальное целое
+*/
+
+import org.w3c.dom.ls.LSOutput;
+
+public class Solution {
+    int num = Integer.MAX_VALUE;
+    byte z = Byte.MAX_VALUE;
+    short x = Short.MAX_VALUE;
+    long c = Long.MAX_VALUE;
+}
+
