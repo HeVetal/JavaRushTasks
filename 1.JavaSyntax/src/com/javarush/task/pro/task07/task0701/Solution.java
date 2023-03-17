8441302228dad1541eb864099f8a1488ed8d17d0
HeVetal <heavyve@gmail.com>
2023-03-17 19:02:56 +0300

    add solution JavaSyntax task 0701

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0701/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task07.task0701;
+
+/* 
+Все возможные типы
+*/
+
+public class Solution {
+    int num;
+    short x;
+    byte z;
+    long c;
+    boolean isTrue;
+    double v;
+    float b;
+    char letter;
+}
