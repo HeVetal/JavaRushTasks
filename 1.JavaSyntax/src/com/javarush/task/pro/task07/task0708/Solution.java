a0e178942b171c9564b046664078b2748e1cd698
HeVetal <heavyve@gmail.com>
2023-03-19 12:14:23 +0300

    add solution JavaSyntax task 0708

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0708/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.pro.task07.task0708;
+
+/* 
+Правильные типы
+*/
+
+public class Solution {
+    public static byte a;
+    public static short b;
+    public static int c;
+    public static long d;
+
+    public static void main(String[] args) {
+
+        setValues(100);
+    }
+
+    public static void setValues(long value){
+        //напишите тут ваш код
+        a = (byte) value;
+        b = (short) value;
+        c = (int)value;
+        d = value;
+    }
+}
