c6e693792987b6e0cffe57b41468ded191850de1
HeVetal <heavyve@gmail.com>
2023-03-19 11:28:24 +0300

    add solution JavaSyntax task 0712

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0712/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.task.pro.task07.task0712;
+
+/* 
+Первые объекты
+*/
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        String a = new String();
+        String q = new String();
+        String w = new String();
+        String e = new String();
+        String r = new String();
+        String t = new String();
+        String y = new String();
+        String u = new String();
+        String i = new String();
+        String o = new String();
+
+        int[] one = new int[1];
+        int[] two = new int[1];
+        int[] three = new int[1];
+        int[] four = new int[1];
+        int[] five = new int[1];
+
+        Scanner scanner = new Scanner(System.in);
+        Scanner console = new Scanner(System.in);
+    }
+}
