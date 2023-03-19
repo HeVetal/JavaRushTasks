fce9038509dc1ca080c468ada5b6aaed7d606b5c
HeVetal <heavyve@gmail.com>
2023-03-19 11:21:34 +0300

    add solution JavaSyntax task 0719

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0719/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.pro.task07.task0719;
+
+/* 
+Космическая Робосутра
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        AutomaticKitchen kitchen = new AutomaticKitchen();
+        Gun gun = new Gun();
+        ProtectiveFieldGenerator generator = new ProtectiveFieldGenerator();
+        RepairDrone drone = new RepairDrone();
+        RobotCleaner robotCleaner = new RobotCleaner();
+    }
+}
