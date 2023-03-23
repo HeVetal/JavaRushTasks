d08d52f315f6f73639419373b410ac8a694acba2
HeVetal <heavyve@gmail.com>
2023-03-23 21:58:54 +0300

    add solution JavaSyntax task 0820

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task08/task0820/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.pro.task08.task0820;
+
+/* 
+Дубликаты на корабле
+*/
+
+import com.javarush.task.pro.task08.task0820.dup1.Diego;
+
+public class Solution {
+    public static void main(String[] args) {
+        Diego diego = new Diego();
+    }
+}
