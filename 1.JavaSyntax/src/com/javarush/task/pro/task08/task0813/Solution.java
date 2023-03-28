c080d14899d7506b400542922180c207578b8151
HeVetal <heavyve@gmail.com>
2023-03-28 10:04:38 +0300

    add solution JavaSyntax task 0813

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task08/task0813/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.pro.task08.task0813;
+
+/* 
+Степень двойки
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println(getPowerOfTwo(3));
+    }
+
+    public static int getPowerOfTwo(int power) {
+        int result = 2;
+        result = result << (power-1);
+        return result;
+    }
+}
