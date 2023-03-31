4313b46260baedf1666abfa636aa18a0671444b6
HeVetal <heavyve@gmail.com>
2023-03-31 22:47:49 +0300

    add solution JavaSyntax task 0922 not finish

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task09/task0922/PiratesWorkbench.java
@@ -0,0 +1,52 @@
+package com.javarush.task.pro.task09.task0922;
+
+/* 
+Закосить под пиратов
+*/
+
+import java.util.Arrays;
+
+public class PiratesWorkbench {
+
+    public static String[] piratesDB = new String[]{
+            "Drax",
+            "Yondu Udonta",
+            "Maz Kanata",
+            "キャプテン・ハーロック",
+            "Star-Lord",
+            "Christopher Summers",
+            "Крысс",
+            "Весельчак У",
+            "Nebula",
+            "Rocket",
+            "Han Solo",
+            "boba Fett"
+    };
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(piratesDB));
+
+        corruptDB();
+        addTwoNames();
+
+        System.out.println(Arrays.toString(piratesDB));
+    }
+
+    public static void corruptDB() {
+        //напишите тут ваш код
+        for (int i = 0; i < piratesDB.length; i++) {
+            if(piratesDB[i].contains("b")){
+                System.out.println(piratesDB[i]);
+                piratesDB[i].replaceAll("b","!");
+            }
+        }
+
+    }
+
+    public static void addTwoNames() {
+        String[] newArray = Arrays.copyOf(piratesDB, piratesDB.length + 2);
+        piratesDB = newArray;
+        piratesDB[piratesDB.length - 2] = "Рыжий Амиго";
+        piratesDB[piratesDB.length - 1] = "Одноглазый Диего";
+    }
+}
