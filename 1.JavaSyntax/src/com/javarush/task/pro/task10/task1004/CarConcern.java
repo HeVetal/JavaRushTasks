d99eb010ceca9b7d90be6d0451385361f6e98224
HeVetal <heavyve@gmail.com>
2023-04-05 10:22:32 +0300

    add solution JavaSyntax task 1004

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1004/CarConcern.java
@@ -0,0 +1,31 @@
+package com.javarush.task.pro.task10.task1004;
+
+/* 
+Жажда скорости
+*/
+
+public class CarConcern {
+    private final String manufacturer = "Lamborghini";
+    private final String model;
+    private final int year;
+    private final String color;
+
+    public CarConcern(String model, int year, String color) {
+        this.model = model;
+        this.year = year;
+        this.color = color;
+    }
+
+    public CarConcern(String model, int year) {
+        this.model = model;
+        this.year = year;
+        this.color = "Оранжевый";
+    }
+
+    public CarConcern(String model) {
+        //напишите тут ваш код
+        this.model = model;
+        this.year = 4321;
+        this.color = "Оранжевый";
+    }
+}
