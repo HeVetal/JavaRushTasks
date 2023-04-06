0722d4cae257071bc5baa523f4c73b8a95d92a0f
HeVetal <heavyve@gmail.com>
2023-04-06 22:27:42 +0300

    add solution JavaSyntax task 1006

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1006/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.pro.task10.task1006;
+
+/* 
+Кто тут наследник?
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Car electric = new ElectricCar();
+        GasCar gas = new GasCar();
+        HybridCar hybrid = new HybridCar();
+
+    }
+}
