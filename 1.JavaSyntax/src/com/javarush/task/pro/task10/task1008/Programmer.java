04dfcd5d5c2b1cc51cfa249e3e59f0a095a50e84
HeVetal <heavyve@gmail.com>
2023-04-07 22:07:47 +0300

    add solution JavaSyntax task 1008

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1008/Programmer.java
@@ -0,0 +1,22 @@
+package com.javarush.task.pro.task10.task1008;
+
+/* 
+Зарплата
+*/
+
+public class Programmer {
+    private int salary = 1000;
+
+    //напишите тут ваш код
+
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        if(salary > this.salary) {
+            this.salary = salary;
+        }
+    }
+}
