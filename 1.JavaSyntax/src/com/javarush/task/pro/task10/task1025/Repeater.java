c98bec130f289f7deb90e59c5445e7785e43b6fa
HeVetal <heavyve@gmail.com>
2023-04-07 22:39:21 +0300

    add solution JavaSyntax task 1025

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1025/Repeater.java
@@ -0,0 +1,29 @@
+package com.javarush.task.pro.task10.task1025;
+
+public class Repeater {
+    private String trajectory = "parabolic";
+    private int frequency = 30;
+
+    public void setTrajectory(String trajectory) {
+
+        this.trajectory = trajectory;
+    }
+
+    public String getTrajectory() {
+        return trajectory;
+    }
+
+    public void setFrequency(int frequency) {
+        this.frequency = frequency;
+
+    }
+
+    public int getFrequency() {
+        return frequency;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Repeater trajectory is %s, with a %dGHz communication frequency.", trajectory, frequency);
+    }
+}
