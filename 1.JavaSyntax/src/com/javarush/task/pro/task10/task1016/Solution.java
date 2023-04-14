e11a48d5add90298c9e38a7e51118f4e85dae47d
HeVetal <heavyve@gmail.com>
2023-04-14 20:41:34 +0300

    add solution JavaSyntax task 1016

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1016/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.pro.task10.task1016;
+
+/* 
+Прогноз погоды
+*/
+
+public class Solution {
+
+    public static void showWeather(City city) {
+        System.out.println(String.format(
+                "В городе %s сегодня температура воздуха %d", city.getName(), city.getTemperature()));
+    }
+
+    public static void main(String[] args) {
+        City city = new City("LALA", 36);
+        showWeather(city);
+    }
+}
