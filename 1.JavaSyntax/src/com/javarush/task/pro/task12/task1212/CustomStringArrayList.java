d3dc9b9a8f26a73ace51e195ccb8e7464a95fceb
Vetal <heavyve@gmail.com>
2023-05-17 23:15:26 +0300

    add solution JavaSyntax task 1212

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task12/task1212/CustomStringArrayList.java
@@ -0,0 +1,38 @@
+package com.javarush.task.pro.task12.task1212;
+
+/* 
+Создаем свой список
+*/
+
+import java.util.Arrays;
+
+public class CustomStringArrayList {
+
+    private int size;
+    private int capacity;
+    private String[] elements;
+
+    public CustomStringArrayList() {
+        capacity = 10;
+        size = 0;
+        elements = new String[capacity];
+    }
+
+    public void add(String element) {
+        if (size == capacity) {
+            grow();
+        }
+        elements[size] = element;
+        size++;
+    }
+
+    private void grow() {
+        //напишите тут ваш код
+        String [] var = Arrays.copyOf(elements, capacity + capacity / 2);
+        elements = Arrays.copyOf(var, var.length);
+        capacity = var.length;
+
+
+    }
+
+}
