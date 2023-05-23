daca3fd0cc4e097f6c25de66028ed8ce606e830d
Vetal <heavyve@gmail.com>
2023-05-23 22:49:11 +0300

    add solution JavaSyntax task 1312

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task13/task1312/Solution.java
@@ -0,0 +1,33 @@
+package com.javarush.task.pro.task13.task1312;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/* 
+ArrayList vs HashMap
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println(getProgrammingLanguages());
+    }
+
+    public static HashMap<Integer, String> getProgrammingLanguages() {
+        HashMap<Integer, String> programmingLanguagesMap = new HashMap<>();
+        ArrayList<String> programmingLanguages = new ArrayList<>();
+        programmingLanguages.add("Java");
+        programmingLanguages.add("Kotlin");
+        programmingLanguages.add("Go");
+        programmingLanguages.add("Javascript");
+        programmingLanguages.add("Typescript");
+        programmingLanguages.add("Python");
+        programmingLanguages.add("PHP");
+        programmingLanguages.add("C++");
+        for (int i = 0; i < programmingLanguages.size(); i++) {
+            programmingLanguagesMap.put(i, programmingLanguages.get(i));
+        }
+        return programmingLanguagesMap;
+    }
+
+}
