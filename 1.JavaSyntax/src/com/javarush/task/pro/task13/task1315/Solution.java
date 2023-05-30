58b9d754fbdf3f8efa4d41ed61a1e6cc18bcbfe6
Vetal <heavyve@gmail.com>
2023-05-30 23:32:38 +0300

    add solution JavaSyntax task 1315

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task13/task1315/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.pro.task13.task1315;
+
+/* 
+Четыре сезона
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        System.out.println(Season.WINTER);
+        System.out.println(Season.SPRING);
+        System.out.println(Season.SUMMER);
+        System.out.println(Season.AUTUMN);
+    }
+}
+//enum Season{
+//    WINTER,
+//    SPRING,
+//    SUMMER,
+//    AUTUMN
+//}
