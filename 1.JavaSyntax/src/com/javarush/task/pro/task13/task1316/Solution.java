f3d1ae50d5d97241308e161be8ae5e893a150bbd
Vetal <heavyve@gmail.com>
2023-05-31 21:37:10 +0300

    add solution JavaSyntax task 1316

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task13/task1316/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.pro.task13.task1316;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        JavarushQuest[]quests = JavarushQuest.values();
+        for (JavarushQuest quest : quests) {
+            System.out.println(quest.ordinal());
+//            System.out.print(quest.ordinal() + " "); красиво но неправильно
+//            System.out.println(quest);
+        }
+    }
+}
