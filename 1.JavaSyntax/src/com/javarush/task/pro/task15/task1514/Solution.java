336dab22f87d5e7956ffa3fe60a562dcc357e9fd
HeVetal <heavyve@gmail.com>
2023-06-12 23:41:48 +0300

    add solution JavaSyntax task 1514

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task15/task1514/Solution.java
@@ -0,0 +1,26 @@
+package com.javarush.task.pro.task15.task1514;
+
+import java.nio.file.Path;
+import java.util.Scanner;
+
+/* 
+Все относительно
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+            Scanner scanner = new Scanner(System.in);
+            String str1 = scanner.nextLine();
+            String str2 = scanner.nextLine();
+            Path path1 = Path.of(str1);
+            Path path2 = Path.of(str2);
+        try {
+            Path result = path1.relativize(path2);
+            System.out.println(result);
+        } catch (Exception ignored){
+
+        }
+    }
+}
+
