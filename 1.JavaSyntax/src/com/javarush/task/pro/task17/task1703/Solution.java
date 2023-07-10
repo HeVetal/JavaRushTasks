543f9245d73a873ed5ed7d76a28887772b726ee8
HeVetal <heavyve@gmail.com>
2023-07-10 15:18:21 +0300

    add solution JavaSyntax task 1702

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task17/task1703/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.pro.task17.task1703;
+
+import java.util.ArrayList;
+
+/* 
+Космическая одиссея ч.1
+*/
+
+public class Solution {
+    public static ArrayList<Astronaut> astronauts = new ArrayList<>();
+
+    public static void main(String[] args) {
+        createCrew();
+        printCrewInfo();
+    }
+
+    public static void createCrew(){
+        //напишите тут ваш код
+        astronauts.add(new Cat());
+        astronauts.add(new Dog());
+        astronauts.add(new Human());
+        astronauts.add(new Human());
+    }
+
+    public static void printCrewInfo() {
+        System.out.println("На борт погружены члены экипажа: ");
+        for (Astronaut astronaut : astronauts) {
+            System.out.println(astronaut.getInfo());
+        }
+    }
+}
