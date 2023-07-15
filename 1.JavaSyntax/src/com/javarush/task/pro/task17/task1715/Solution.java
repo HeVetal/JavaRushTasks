62759da6eb7ce2f80377bd16748a483bb85147e4
Vetal <heavyve@gmail.com>
2023-07-15 22:14:58 +0300

    add solution JavaSyntax task 1715

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task17/task1715/Solution.java
@@ -0,0 +1,41 @@
+package com.javarush.task.pro.task17.task1715;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+Вилларибо и Виллабаджо.
+*/
+
+public class Solution {
+    static List<MusicalInstrument> orchestra = new ArrayList<>();
+
+    public static void main(String[] args) {
+        createKeyboardOrchestra();
+        createStringedOrchestra();
+        playOrchestra();
+    }
+
+    public static void createKeyboardOrchestra() {
+        //напишите тут ваш код
+        orchestra.add(new Organ());
+        orchestra.add(new Piano());
+        orchestra.add(new Piano());
+        orchestra.add(new Piano());
+
+    }
+
+    public static void createStringedOrchestra() {
+        //напишите тут ваш код
+        orchestra.add(new Violin());
+        orchestra.add(new Violin());
+        orchestra.add(new Guitar());
+    }
+
+    public static void playOrchestra() {
+        //напишите тут ваш код
+        for (MusicalInstrument musicalInstrument : orchestra) {
+            musicalInstrument.play();
+        }
+    }
+}
