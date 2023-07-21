f540f4912b1ad0c4dfd711958db1dae522f53b37
Vetal <heavyve@gmail.com>
2023-07-21 20:28:06 +0300

    add solution JavaSyntax task 1208

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1208/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.task.task12.task1208;
+
+/* 
+Свобода печати
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    static double print(double d){
+        return 0.0;
+    }
+
+    static int print(int i){
+        return 0;
+    }
+
+    static short print(short i){
+        return 0;
+    }
+
+    static boolean print(boolean i){
+        return true;
+    }
+
+
+    static long print(long i){
+        return 0;
+    }
+}
