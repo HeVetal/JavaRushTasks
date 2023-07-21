5be151bf5ed0d866324289cdf9c5a43d95cb4edc
HeVetal <heavyve@gmail.com>
2023-07-21 16:29:08 +0300

    add solution JavaSyntax task 1309

---
+++ 2.JavaCore/src/com/javarush/task/task13/task1309/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task13.task1309;
+
+/* 
+Всё, что движется
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+    }
+    public interface CanMove{
+        Double speed();
+    }
+
+    public interface CanFly extends CanMove{
+        default Double speed(CanFly canFly){
+            return this.speed();
+        }
+    }
+}
