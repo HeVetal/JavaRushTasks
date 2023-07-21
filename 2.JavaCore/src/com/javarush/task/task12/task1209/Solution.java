56314bf71a5f57540923374c5458b16a21f1d26f
Vetal <heavyve@gmail.com>
2023-07-21 20:31:15 +0300

    add solution JavaSyntax task 1209

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1209/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task12.task1209;
+
+/* 
+Три метода и минимум
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    //Напишите тут ваши методы
+    public static int min(int a, int b){
+        return Math.min(a,b);
+    }
+
+    public static long min(long a, long b){
+        return Math.min(a,b);
+    }
+
+    public static double min(double a, double b){
+        return Math.min(a,b);
+    }
+}
