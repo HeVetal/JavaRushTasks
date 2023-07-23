9f1ba7473bc253d20c9bb0fe4b18159e527df579
Vetal <heavyve@gmail.com>
2023-07-23 14:42:53 +0300

    add solution JavaSyntax task 1207

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1207/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.task12.task1207;
+
+/* 
+Int и Integer
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        print(1);
+        print((Integer) 2);
+    }
+
+    //Напишите тут ваши методы
+    public static void print(int i){
+
+    }
+
+    public static void print(Integer z){
+
+    }
+}
