e250424810f888d80a6070d91ebc5689f5c3a4fb
Vetal <heavyve@gmail.com>
2023-07-23 14:38:38 +0300

    add solution JavaSyntax task 1206

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1206/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.task12.task1206;
+
+/* 
+Выполняем перегрузку!
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    //Напишите тут ваши методы
+    public static int print(int i){
+        return 0;
+    }
+
+    public static String print(String str){
+        return null;
+    }
+}
