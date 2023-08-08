8bd59451811b22478eb867108ccceee969ab8fab
HeVetal <heavyve@gmail.com>
2023-08-08 15:09:54 +0300

    add solution JavaCore task 1328

---
+++ 2.JavaCore/src/com/javarush/task/task13/task1328/AbstractRobot.java
@@ -0,0 +1,54 @@
+package com.javarush.task.task13.task1328;
+
+
+public abstract class AbstractRobot implements Attackable, Defensable {
+
+    private int hitCount;
+
+    private String name;
+
+    public AbstractRobot(String name) {
+
+        this.name = name;
+    }
+
+    public String getName() {
+
+        return name;
+    }
+
+    public BodyPart attack() {
+        BodyPart attackedBodyPart = null;
+        hitCount = hitCount + 1;
+
+        if (hitCount == 1) {
+            attackedBodyPart = BodyPart.ARM;
+        } else if (hitCount == 2) {
+            attackedBodyPart = BodyPart.HEAD;
+        } else if (hitCount == 3) {
+            attackedBodyPart = BodyPart.LEG;
+        } else if (hitCount == 4) {
+            hitCount = 0;
+            attackedBodyPart = BodyPart.CHEST;
+
+        }
+        return attackedBodyPart;
+    }
+
+    public BodyPart defense() {
+        BodyPart defendedBodyPart = null;
+        hitCount = hitCount + 2;
+
+        if (hitCount == 1) {
+            defendedBodyPart = BodyPart.HEAD;
+        } else if (hitCount == 2) {
+            defendedBodyPart = BodyPart.LEG;
+        } else if (hitCount == 3) {
+            defendedBodyPart = BodyPart.CHEST;
+        } else if (hitCount == 4) {
+            hitCount = 0;
+            defendedBodyPart = BodyPart.ARM;;
+        }
+        return defendedBodyPart;
+    }
+}
---
+++ 2.JavaCore/src/com/javarush/task/task13/task1328/Robot.java
@@ -0,0 +1,11 @@
+package com.javarush.task.task13.task1328;
+
+import java.util.Random;
+
+public class Robot extends AbstractRobot {
+
+    public Robot(String name){
+        super(name);
+    }
+
+}
