1af168f6be4233eb39dfe426cf9b98342168459d
HeVetal <heavyve@gmail.com>
2023-08-08 13:55:17 +0300

    add solution JavaCore task 1307

---
+++ 2.JavaCore/src/com/javarush/task/task13/task1307/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task13.task1307;
+
+/* 
+Параметризованый интерфейс
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+    }
+
+    interface SimpleObject<String> {
+        SimpleObject<String> getInstance();
+    }
+
+    class StringObject implements SimpleObject<String>//допишите здесь ваш код
+    {
+
+        @Override
+        public SimpleObject<String> getInstance() {
+
+            return null;
+        }
+    }
+}
