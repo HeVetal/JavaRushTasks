8a5c9189579afceb4c4a7dfe6138fea35f26b35f
HeVetal <heavyve@gmail.com>
2023-09-05 16:10:09 +0300

    add solution JavaCore task 1706

---
+++ 2.JavaCore/src/com/javarush/task/task17/task1706/OurPresident.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task17.task1706;
+
+public class OurPresident {
+
+    static {
+        synchronized (OurPresident.class) {
+            president = new OurPresident();
+        }
+    }
+
+    private static OurPresident president;
+
+    private OurPresident() {
+    }
+
+    public static OurPresident getOurPresident() {
+        return president;
+    }
+}
