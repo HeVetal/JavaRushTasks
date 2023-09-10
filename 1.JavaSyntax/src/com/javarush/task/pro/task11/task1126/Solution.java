c4408bcc547b40ac590b5619b10e1a38471e57aa
Vetal <heavyve@gmail.com>
2023-09-10 15:52:17 +0300

    add solution JavaSyntax task1126

--- 1.JavaSyntax/src/com/javarush/task/pro/task11/task1126/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task11/task1126/Solution.java
@@ -1,6 +1,6 @@
 package com.javarush.task.pro.task11.task1126;
 
-/* 
+/*
 Студент
 */
 
---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task11/task1126/Student.java
@@ -0,0 +1,10 @@
+package com.javarush.task.pro.task11.task1126;
+
+public class Student {
+
+    String name;
+
+    Student(String name){
+        this.name = name;
+    }
+}
