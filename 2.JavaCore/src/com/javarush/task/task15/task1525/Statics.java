1a965cf6598c0d29e24d3702f430ad3322b11748
HeVetal <heavyve@gmail.com>
2023-09-25 13:23:44 +0300

    add solution JavaCore task 1525

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1525/Statics.java
@@ -0,0 +1,6 @@
+package com.javarush.task.task15.task1525;
+
+public class Statics {
+    public static String FILE_NAME =
+            "C:\\Users\\User\\IdeaProjects\\JavaRushTasks\\2.JavaCore\\src\\com\\javarush\\task\\task15\\task1525\\test";
+}
