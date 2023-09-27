29ba70335f6a47ed79db0e9c868525f4ceaf353b
Vetal <heavyve@gmail.com>
2023-09-27 17:11:44 +0300

    add solution JavaCore task1809 SneakyThrows

--- 2.JavaCore/src/com/javarush/task/task18/task1809/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1809/Solution.java
@@ -1,7 +1,10 @@
 package com.javarush.task.task18.task1809;
 
+import lombok.SneakyThrows;
+
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -10,25 +13,22 @@ import java.util.List;
 */
 
 public class Solution {
+    @SneakyThrows
     public static void main(String[] args) {
-        List<Byte> list = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-             FileInputStream inputStream = new FileInputStream(reader.readLine())) {
+             FileInputStream inputStream = new FileInputStream(reader.readLine());
+             FileOutputStream outputStream = new FileOutputStream(reader.readLine())) {
             while (inputStream.available() > 0) {
-                list.add((byte) inputStream.read());
+                list.add(inputStream.read());
             }
             Collections.reverse(list);
-            FileOutputStream outputStream = new FileOutputStream(reader.readLine());
-            list.forEach(s -> {
-                try {
-                    outputStream.write(s);
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            });
-            outputStream.close();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+            list.forEach(i -> write(outputStream, i));
         }
+    }
+
+    @SneakyThrows
+    private static void write(OutputStream outputStream, int index) {
+        outputStream.write(index);
     }
 }
