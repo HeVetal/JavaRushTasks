74ff6491737a4dc43fb5bfb3fb3b5814ada8dba0
Vetal <heavyve@gmail.com>
2023-09-30 19:21:52 +0300

    add solution JavaCore task1912

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1912/Solution.java
@@ -0,0 +1,30 @@
+package com.javarush.task.task19.task1912;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+/* 
+Ридер обертка 2
+*/
+
+public class Solution {
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args) {
+        PrintStream consoleStream = System.out;
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream stream = new PrintStream(outputStream);
+        System.setOut(stream);
+        testString.printSomething();
+        String result = outputStream.toString();
+        System.setOut(consoleStream);
+        result = result.replaceAll("te", "??");
+        System.out.println(result);
+    }
+
+    public static class TestString {
+        public void printSomething() {
+            System.out.println("it's a text for testing");
+        }
+    }
+}
