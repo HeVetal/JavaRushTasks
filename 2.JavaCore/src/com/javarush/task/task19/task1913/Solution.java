60aa0ca5fa68aeb2a732dbad0baf080f4f4e6822
Vetal <heavyve@gmail.com>
2023-09-30 19:35:38 +0300

    add solution JavaCore task1913

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1913/Solution.java
@@ -0,0 +1,30 @@
+package com.javarush.task.task19.task1913;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+/* 
+Выводим только цифры
+*/
+
+public class Solution {
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args) {
+        PrintStream consoleStream = System.out;
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream stream = new PrintStream(outputStream);
+        System.setOut(stream);
+        testString.printSomething();
+        String result = outputStream.toString();
+        System.setOut(consoleStream);
+        result = result.replaceAll("\\D", "");
+        System.out.println(result);
+    }
+
+    public static class TestString {
+        public void printSomething() {
+            System.out.println("it's 1 a 23 text 4 f5-6or7 tes8ting");
+        }
+    }
+}
