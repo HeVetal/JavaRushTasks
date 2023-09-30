c5ce420157f7c92b1f64e8e6b1020a6cf187a131
Vetal <heavyve@gmail.com>
2023-09-30 19:17:37 +0300

    add solution JavaCore task1911

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1911/Solution.java
@@ -0,0 +1,33 @@
+package com.javarush.task.task19.task1911;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+/* 
+Ридер обертка
+*/
+
+public class Solution {
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args) {
+        PrintStream consoleStream = System.out;
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream stream = new PrintStream(outputStream);
+        System.setOut(stream);
+        testString.printSomething();
+        String result = outputStream.toString();
+        System.setOut(consoleStream);
+
+        result = result.toUpperCase();
+
+        System.out.println(result);
+    }
+
+    public static class TestString {
+        public void printSomething() {
+            System.out.println("it's a text for testing");
+        }
+    }
+}
