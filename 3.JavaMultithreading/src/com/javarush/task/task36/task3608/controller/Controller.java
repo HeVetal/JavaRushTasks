f89954902dcd77a89be7c55a51bdc8f410c1407a
Vetal <heavyve@gmail.com>
2023-10-04 22:10:26 +0300

    add solution JavaMultithreading task3608 MVC 3/9

--- 3.JavaMultithreading/src/com/javarush/task/task36/task3608/controller/Controller.java
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/controller/Controller.java
@@ -1,9 +1,11 @@
 package com.javarush.task.task36.task3608.controller;
 
 import com.javarush.task.task36.task3608.model.Model;
+import com.javarush.task.task36.task3608.view.UsersView;
 
 public class Controller {
     private Model model;
+    private UsersView usersView;
 
     public void setModel(Model model) {
         this.model = model;
@@ -11,5 +13,10 @@ public class Controller {
 
     public void onShowAllUsers() {
         model.loadUsers();
+        usersView.refresh(model.getModelData());
+    }
+
+    public void setUsersView(UsersView usersView) {
+        this.usersView = usersView;
     }
 }
