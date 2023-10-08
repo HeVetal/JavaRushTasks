93b387392003ae7eae621088eda8ad44b7e5b3a4
Vetal <heavyve@gmail.com>
2023-10-08 14:36:20 +0300

    add solution JavaMultithreading task3608 6/9

--- 3.JavaMultithreading/src/com/javarush/task/task36/task3608/view/EditUserView.java
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/view/EditUserView.java
@@ -4,31 +4,19 @@ import com.javarush.task.task36.task3608.bean.User;
 import com.javarush.task.task36.task3608.controller.Controller;
 import com.javarush.task.task36.task3608.model.ModelData;
 
-import java.util.List;
-
-public class UsersView implements View {
+public class EditUserView implements View{
     private Controller controller;
-
 
     @Override
     public void refresh(ModelData modelData) {
-        System.out.println(modelData.isDisplayDeletedUserList() ? "All deleted users:" : "All users:");
-        modelData.getUsers().forEach(user -> System.out.println("\t" + user));
-        //users.stream().map(user -> "\t" + user).forEach(System.out::println);
-
+        System.out.println("User to be edited:");
+        System.out.println("\t" + modelData.getActiveUser());
         System.out.println("===================================================");
+
     }
 
     @Override
     public void setController(Controller controller) {
         this.controller = controller;
-    }
-
-    public void fireEventShowAllUsers() {
-        controller.onShowAllUsers();
-    }
-
-    public void fireEventShowDeletedUsers() {
-        controller.onShowAllDeletedUsers();
     }
 }
