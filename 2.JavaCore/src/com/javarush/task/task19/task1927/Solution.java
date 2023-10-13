b8374de89d8263d7b3e1ef2a1d9c2923f5efd2e3
HeVetal <heavyve@gmail.com>
2023-10-13 12:40:24 +0300

    add solution JavaCore task 1927

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1927/Solution.java
@@ -0,0 +1,37 @@
+package com.javarush.task.task19.task1927;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+/* 
+Контекстная реклама
+*/
+
+public class Solution {
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args) {
+        PrintStream console = System.out;
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+        testString.printSomething();
+        System.setOut(console);
+        String[] split = outputStream.toString().split("\\n");
+        for (int i = 0; i < split.length; i++) {
+            if (i % 2 == 0 && i != 0) {
+                System.out.println("JavaRush - курсы Java онлайн");
+            }
+            System.out.println(split[i]);
+        }
+    }
+
+    public static class TestString {
+        public void printSomething() {
+            System.out.println("first");
+            System.out.println("second");
+            System.out.println("third");
+            System.out.println("fourth");
+            System.out.println("fifth");
+        }
+    }
+}
