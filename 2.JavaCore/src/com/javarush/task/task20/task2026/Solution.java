ebeba817971b72519f95f15cd2747ad9ffe2804d
Vetal <heavyve@gmail.com>
2023-10-28 22:39:31 +0300

    add solution JavaCore task 2026

---
+++ 2.JavaCore/src/com/javarush/task/task20/task2026/Solution.java
@@ -0,0 +1,47 @@
+package com.javarush.task.task20.task2026;
+
+/* 
+Алгоритмы-прямоугольники
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        byte[][] a1 = new byte[][]{
+                {1, 1, 0, 0},
+                {1, 1, 0, 0},
+                {1, 1, 0, 0},
+                {1, 1, 0, 1}
+        };
+        byte[][] a2 = new byte[][]{
+                {1, 0, 0, 1},
+                {0, 0, 0, 0},
+                {0, 0, 0, 0},
+                {1, 0, 0, 1}
+        };
+
+        int count1 = getRectangleCount(a1);
+        System.out.println("count = " + count1 + ". Должно быть 2");
+        int count2 = getRectangleCount(a2);
+        System.out.println("count = " + count2 + ". Должно быть 4");
+    }
+
+    public static int getRectangleCount(byte[][] a) {
+        int answer = 0;
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[0].length; j++) {
+                if (a[i][j] == 1) {
+                    if (i < a.length - 1 && a[i + 1][j] == 1) {
+                        a[i][j] = 0;
+                    }
+                    if (j < a[0].length - 1 && a[i][j + 1] == 1) {
+                        a[i][j] = 0;
+                    }
+                    if (a[i][j] == 1) {
+                        answer++;
+                    }
+                }
+            }
+        }
+        return answer;
+    }
+}
