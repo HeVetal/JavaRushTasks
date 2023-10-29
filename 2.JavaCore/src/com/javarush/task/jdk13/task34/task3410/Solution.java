3f41e0bea01b5adeb06f312c99f6004e41443170
Vetal <heavyve@gmail.com>
2023-10-29 21:32:10 +0300

    add solution JavaCore task 3410

---
+++ 2.JavaCore/src/com/javarush/task/jdk13/task34/task3410/Solution.java
@@ -0,0 +1,30 @@
+package com.javarush.task.jdk13.task34.task3410;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+
+/* 
+Обнуление объекта
+*/
+
+public class Solution {
+
+    public static void main(String[] args) throws Exception {
+        User user = new User(28219, "Paulo", 40, 9999, "paulo@javarush.com");
+        System.out.println(user);
+        reset(user);
+        System.out.println(user);
+    }
+
+    public static void reset(Object object) throws Exception {
+        Field[] declaredFields = object.getClass().getDeclaredFields();
+        for (Field field : declaredFields) {
+            if (Modifier.isPrivate(field.getModifiers()) &&
+                    !Modifier.isStatic(field.getModifiers()) &&
+                    !field.getType().isPrimitive()) {
+                field.setAccessible(true);
+                field.set(object, null);
+            }
+        }
+    }
+}
