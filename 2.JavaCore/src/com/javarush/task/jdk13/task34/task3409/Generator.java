494b5aa06078602e831dc2f8eb6109f6eb306281
Vetal <heavyve@gmail.com>
2023-10-29 22:32:56 +0300

    add solution JavaCore task 3409 not finish

---
+++ 2.JavaCore/src/com/javarush/task/jdk13/task34/task3409/Generator.java
@@ -0,0 +1,18 @@
+package com.javarush.task.jdk13.task34.task3409;
+
+import java.lang.reflect.Constructor;
+
+public class Generator<T> {
+
+    private Class<T> aClass;
+
+    public Generator(Class<T> aClass) {
+        this.aClass = aClass;
+    }
+
+    T newInstance() throws Exception {
+        //Constructor<T> declaredConstructor = (Constructor<T>) aClass.getDeclaredConstructors()[0];
+        Constructor<T> declaredConstructor = aClass.getDeclaredConstructor();
+        return (T) declaredConstructor.newInstance();
+    }
+}
