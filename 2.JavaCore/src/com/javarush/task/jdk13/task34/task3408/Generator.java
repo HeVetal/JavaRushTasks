5fdc9b1d7bc44d9a09aae7587b87a4686e5e9841
Vetal <heavyve@gmail.com>
2023-10-29 21:22:58 +0300

    add solution JavaCore task 3408

---
+++ 2.JavaCore/src/com/javarush/task/jdk13/task34/task3408/Generator.java
@@ -0,0 +1,15 @@
+package com.javarush.task.jdk13.task34.task3408;
+
+import java.lang.reflect.InvocationTargetException;
+
+public class Generator<T> {
+    private Class<T> aClass;
+
+    public Generator(Class<T> aClass) {
+        this.aClass = aClass;
+    }
+
+    T newInstance() throws Exception {
+        return aClass.getDeclaredConstructor().newInstance();
+    }
+}
