84ce1bf66a8d586d63b2c47bf68ba1fc468d58ac
Vetal <heavyve@gmail.com>
2023-11-01 21:48:37 +0300

    add solution JavaCore task 3509

--- 2.JavaCore/src/com/javarush/task/jdk13/task35/task3509/Solution.java
+++ 2.JavaCore/src/com/javarush/task/jdk13/task35/task3509/Solution.java
@@ -3,6 +3,8 @@ package com.javarush.task.jdk13.task35.task3509;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /* 
 Wildcards для коллекций
@@ -13,38 +15,41 @@ public class Solution {
     public static void main(String[] args) {
     }
 
-    public static Double sum(List list) {
-        Double result = 0.0;
-        for (int i = 0; i < list.size(); i++) {
-            Number numb = (Number) list.get(i);
-            result += numb.doubleValue();
-        }
-        return result;
+    public static Double sum(List<? extends Number> list) {
+//        double result = 0.0;
+//        for (Number numb : list) {
+//            result += numb.doubleValue();
+//        }
+//        return result;
+        return list.stream().mapToDouble(Number::doubleValue).sum();
     }
 
-    public static Double multiply(List list) {
-        Double result = 1.0;
-        for (int i = 0; i < list.size(); i++) {
-            Number numb = (Number) list.get(i);
-            result *= numb.doubleValue();
-        }
-        return result;
+    public static Double multiply(List<? extends Number> list) {
+//        double result = 1.0;
+//        for (Number numb : list) {
+//            result *= numb.doubleValue();
+//        }
+//        return result;
+        return list.stream().mapToDouble(Number::doubleValue).reduce(1, (a, b) -> a * b);
     }
 
-    public static String concat(List list) {
-        StringBuilder builder = new StringBuilder();
-        for (Object obj : list) {
-            builder.append(obj);
-        }
-        return builder.toString();
+    public static String concat(List<?> list) {
+//        StringBuilder builder = new StringBuilder();
+//        for (Object obj : list) {
+//            builder.append(obj);
+//        }
+//        return builder.toString();
+        return list.stream().map(String::valueOf).collect(Collectors.joining());
     }
 
-    public static List combine(List list) {
-        List result = new ArrayList<>();
-        for (int i = 0; i < list.size(); i++) {
-            Collection collection = (Collection) list.get(i);
-            result.addAll(collection);
-        }
-        return result;
+    public static <T> List<T> combine(List<? extends Collection<T>> list) {
+//        List <T>result = new ArrayList<>();
+//        for (Collection<T> collection : list) {
+//            result.addAll(collection);
+//        }
+//        return result;
+        Stream<? extends Collection<T>> stream = list.stream();
+        Stream<T> tStream = stream.flatMap(Collection::stream);
+        return tStream.toList();
     }
 }
