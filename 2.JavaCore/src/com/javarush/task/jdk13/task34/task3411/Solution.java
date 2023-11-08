789a85ff3957bf34e9f3f8c33fd82cb895e8bc63
Vetal <heavyve@gmail.com>
2023-11-08 20:39:28 +0300

    add solution JavaCore task 3411

---
+++ 2.JavaCore/src/com/javarush/task/jdk13/task34/task3411/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.jdk13.task34.task3411;
+
+import java.lang.reflect.Field;
+
+/* 
+Приватный родитель
+*/
+
+public class Solution {
+
+    public static void main(String[] args) throws Exception {
+        Child child = Child.class.getDeclaredConstructor().newInstance();
+        System.out.println(child.getPrivateField());
+        setPrivateParentField(child, "privateField", "новая строка");
+        System.out.println(child.getPrivateField());
+    }
+
+    public static void setPrivateParentField(Object childObject, String fieldName, Object value) throws Exception {
+        Field field = childObject.getClass().getSuperclass().getDeclaredField(fieldName);
+        field.setAccessible(true);
+        field.set(childObject, value);
+    }
+}
