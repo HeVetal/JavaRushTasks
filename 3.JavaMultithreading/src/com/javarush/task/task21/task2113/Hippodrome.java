8abd13a0e0be78bcf478b8ac960dccadf4225ef3
HeVetal <heavyve@gmail.com>
2023-11-09 13:19:05 +0300

    add solution JavaMultithreading task 2113/8

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
@@ -15,8 +15,12 @@ public class Hippodrome {
         return horses;
     }
 
-    void run(){
-
+    void run() throws InterruptedException {
+        for (int i = 0; i < 100; i++) {
+            move();
+            print();
+            Thread.sleep(200);
+        }
     }
     void move(){
 
