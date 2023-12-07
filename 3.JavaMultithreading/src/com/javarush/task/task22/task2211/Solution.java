75e2718c7e6136a235bfc238f509eaaab2b87d6b
HeVetal <heavyve@gmail.com>
2023-12-07 16:17:10 +0300

    add solution JavaMultithreading task 2211

---
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2211/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task22.task2211;
+
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+Смена кодировки
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(args[0]), "Windows-1251"));
+             BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(args[1]), "UTF-8"))) {
+            while (reader.ready()) {
+                writer.write(reader.readLine());
+                writer.write(System.lineSeparator());
+
+            }
+        }
+    }
+}
