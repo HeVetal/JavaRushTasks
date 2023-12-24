6b4c96700a2ca82ec31238c99621c05e5e7dcd6a
Vetal <heavyve@gmail.com>
2023-12-24 19:56:38 +0300

    add solution JavaMultithreading task 2310

---
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2310/Solution.java
@@ -0,0 +1,30 @@
+package com.javarush.task.task23.task2310;
+
+/* 
+Напряги извилины!
+*/
+
+public class Solution {
+    private String name;
+
+    Solution(String name) {
+        this.name = name;
+    }
+
+    protected String getName() {
+        return name;
+    }
+
+    private void sout() {
+        new Solution("The Darkside Hacker") {
+            void printName() {
+                System.out.println(getName());
+            }
+        }.printName();
+    }
+
+    public static void main(String[] args) {
+
+        new Solution("Риша").sout();
+    }
+}
