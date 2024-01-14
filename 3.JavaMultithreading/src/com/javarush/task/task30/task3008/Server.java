022de912fe56fef6e47db6110da22da7701b2da1
Vetal <heavyve@gmail.com>
2024-01-14 22:25:42 +0300

    add solution JavaMultithreading task 3008

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -3,14 +3,29 @@ package com.javarush.task.task30.task3008;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 
 public class Server {
+    private static Map<String, Connection> connectionMap = new ConcurrentHashMap<>();
+
+    public static void sendBroadcastMessage(Message message) {
+        for (Connection connection : connectionMap.values()) {
+            try {
+                connection.send(message);
+            } catch (IOException e) {
+                ConsoleHelper.writeMessage("Не смогли отправить сообщение " + connection.getRemoteSocketAddress());
+            }
+        }
+    }
+
     public static void main(String[] args) {
         ConsoleHelper.writeMessage("Введите порт сервера: ");
         int port = ConsoleHelper.readInt();
         try (ServerSocket serverSocket = new ServerSocket(port)) {
             ConsoleHelper.writeMessage("Чат сервер запущен");
-            while (true){
+            while (true) {
                 Socket socket = serverSocket.accept();
                 Handler handler = new Handler(socket);
                 handler.start();
