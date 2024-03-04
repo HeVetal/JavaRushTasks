6b593ebf5219cc4e3f1c5aa51258b63611b5e122
Vetal <heavyve@gmail.com>
2024-03-04 22:19:44 +0300

    add solution JavaMultithreading task 2209

--- 3.JavaMultithreading/src/com/javarush/task/task22/task2209/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2209/Solution.java
@@ -1,9 +1,7 @@
 package com.javarush.task.task22.task2209;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
 
 /* 
 Составить цепочку слов
@@ -25,20 +23,39 @@ public class Solution {
     }
 
     public static StringBuilder getLine(String... words) {
-        StringBuilder builder = new StringBuilder(words[0]);
-        for (int i = 1; i < words.length; i++) {
-            String firstStart = builder.substring(0, 1);
-            String firstEnd = builder.substring(builder.length() - 1);
-
-            String secondStart = words[i].substring(0, 1);
-            String secondEnd = words[i].substring(words[i].length() - 1);
-
-            if (firstEnd.equalsIgnoreCase(secondStart)) {
-                builder.append(" ").append(words[i]);
-            } else if (firstStart.equalsIgnoreCase(secondEnd)) {
-                builder.insert(0, words[i] + " ");
-            }
+        if (words == null || words.length == 0) {
+            return new StringBuilder();
         }
-        return builder;
+        List<StringBuilder> builderList = new ArrayList<>();
+        for (String word : words) {
+            StringBuilder builder = new StringBuilder(word);
+            List<String> list = new ArrayList<>(Arrays.asList(words));
+            list.remove(word);
+            int count = 0;
+            for (int i = 0; i < list.size(); i++) {
+                String firstStart = builder.substring(0, 1);
+                String firstEnd = builder.substring(builder.length() - 1);
+
+                String secondStart = list.get(i).substring(0, 1);
+                String secondEnd = list.get(i).substring(list.get(i).length() - 1);
+
+                if (firstEnd.equalsIgnoreCase(secondStart)) {
+                    builder.append(" ").append(list.get(i));
+                } else if (firstStart.equalsIgnoreCase(secondEnd)) {
+                    builder.insert(0, list.get(i) + " ");
+                } else {
+                    String string = list.get(i);
+                    list.remove(string);
+                    i--;
+                    list.add(string);
+                    count++;
+                    if (count > 1000) {
+                        break;
+                    }
+                }
+            }
+            builderList.add(builder);
+        }
+        return builderList.stream().max(Comparator.comparingInt(StringBuilder::length)).get();
     }
 }
