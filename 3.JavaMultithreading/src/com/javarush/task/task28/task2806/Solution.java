e2aac149e60a85f40ef0d705b8a6972dce58248a
HeVetal <heavyve@gmail.com>
2024-03-20 13:40:35 +0300

    add solution JavaMultithreading task 2806

---
+++ 3.JavaMultithreading/src/com/javarush/task/task28/task2806/Solution.java
@@ -0,0 +1,38 @@
+package com.javarush.task.task28.task2806;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+/* 
+Знакомство с Executors
+*/
+
+public class Solution {
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+        for (int i = 1; i < 11; i++) {
+            int finalI = i;
+            Runnable task = ()-> doExpensiveOperation(finalI);
+            executorService.submit(task);
+        }
+        executorService.shutdown();
+        executorService.awaitTermination(5, TimeUnit.SECONDS);
+    }
+
+    private static void doExpensiveOperation(int localId) {
+        System.out.println(Thread.currentThread().getName() + ", localId=" + localId);
+    }
+}
+/* output example
+pool-1-thread-2, localId=2
+pool-1-thread-1, localId=1
+pool-1-thread-3, localId=3
+pool-1-thread-1, localId=7
+pool-1-thread-1, localId=9
+pool-1-thread-4, localId=4
+pool-1-thread-5, localId=5
+pool-1-thread-2, localId=6
+pool-1-thread-1, localId=10
+pool-1-thread-3, localId=8
+         */
