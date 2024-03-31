bac5e49b035e32e0f886a7c73795cfc71c6a6b68
Vetal <heavyve@gmail.com>
2024-03-31 10:53:43 +0300

    add solution JavaMultithreading task 2701

---
+++ 3.JavaMultithreading/src/com/javarush/task/task25/task2502/Solution.java
@@ -0,0 +1,41 @@
+package com.javarush.task.task25.task2502;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/* 
+Машину на СТО не повезем!
+*/
+
+public class Solution {
+    public static enum Wheel {
+        FRONT_LEFT,
+        FRONT_RIGHT,
+        BACK_LEFT,
+        BACK_RIGHT
+    }
+
+    public static class Car {
+        protected List<Wheel> wheels;
+
+        public Car() {
+            //init wheels here
+//            Wheel[] values = Wheel.values();
+//            wheels = new ArrayList<>();
+//            for (int i = 0; i < 4; ) {
+//                wheels.get(i) = Wheel.valueOf(String.valueOf(values[i]).toString());
+//                i++;
+//            }
+        }
+
+        protected String[] loadWheelNamesFromDB() {
+            //this method returns mock data
+            return new String[]{"FRONT_LEFT", "FRONT_RIGHT", "BACK_LEFT", "BACK_RIGHT"};
+        }
+    }
+
+    public static void main(String[] args) {
+    }
+}
