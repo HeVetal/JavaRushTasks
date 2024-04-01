44e22d4172c4f24484831e0f22b5d9567587a8df
Vetal <heavyve@gmail.com>
2024-04-01 22:50:13 +0300

    add solution JavaMultithreading task 2805

---
+++ 3.JavaMultithreading/src/com/javarush/task/task28/task2805/MyThread.java
@@ -0,0 +1,48 @@
+package com.javarush.task.task28.task2805;
+
+public class MyThread extends Thread {
+
+
+    static int count = MIN_PRIORITY;
+
+    {
+        setPriority(count++);
+        if(count > MAX_PRIORITY){
+            count = MIN_PRIORITY;
+        }
+
+    }
+
+
+    public MyThread() {
+    }
+
+    public MyThread(Runnable target) {
+        super(target);
+    }
+
+    public MyThread(ThreadGroup group, Runnable target) {
+        super(group, target);
+    }
+
+    public MyThread(String name) {
+        super(name);
+    }
+
+    public MyThread(ThreadGroup group, String name) {
+        super(group, name);
+    }
+
+    public MyThread(Runnable target, String name) {
+        super(target, name);
+    }
+
+    public MyThread(ThreadGroup group, Runnable target, String name) {
+        super(group, target, name);
+    }
+
+    public MyThread(ThreadGroup group, Runnable target, String name, long stackSize) {
+        super(group, target, name, stackSize);
+    }
+
+}
