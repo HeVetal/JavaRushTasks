073faab509c9a7f820b3fd36ab8ed6c770e4f7c1
HeVetal <heavyve@gmail.com>
2024-04-04 16:32:52 +0300

    add solution Core task 2811

---
+++ 2.JavaCore/src/com/javarush/task/jdk13/task28/task2811/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.jdk13.task28.task2811;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
+
+/* 
+Знакомство с Executors
+*/
+
+public class Solution {
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+        IntStream.range(1, 21).mapToObj(i -> (Runnable) () -> doExpensiveOperation(i))
+                .forEach(executorService::submit);
+        executorService.shutdown();
+        executorService.awaitTermination(5, TimeUnit.SECONDS);
+
+    }
+
+    private static void doExpensiveOperation(int localID) {
+        System.out.println(Thread.currentThread().getName() + ", localID=" + localID);
+    }
+}
