dd87be256e5f154f51170381af41c77674fc5c25
HeVetal <heavyve@gmail.com>
2024-04-04 16:33:20 +0300

    add solution Core task 2812

---
+++ 2.JavaCore/src/com/javarush/task/jdk13/task28/task2812/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.jdk13.task28.task2812;
+
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/* 
+ShutdownNow!
+*/
+
+public class Solution {
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executor = Executors.newFixedThreadPool(5);
+        for (int i = 1; i <= 10; i++) {
+            final int localId = i;
+            executor.submit(() -> doExpensiveOperation(localId));
+        }
+
+        List<Runnable> runnables = executor.shutdownNow();
+        Thread.sleep(100);
+        runnables.forEach(runnable -> System.out.println(runnable.toString().substring(0, 40) + "... was not completed"));
+    }
+
+    private static void doExpensiveOperation(int localId) {
+        System.out.println(Thread.currentThread().getName() + ", localId=" + localId);
+    }
+}
