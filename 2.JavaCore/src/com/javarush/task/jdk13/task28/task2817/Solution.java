aec853fb6d73f02bd64c6a4938b82265ab2bb14b
HeVetal <heavyve@gmail.com>
2024-04-08 20:56:14 +0300

    add solution JavaCore task 2817

---
+++ 2.JavaCore/src/com/javarush/task/jdk13/task28/task2817/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.jdk13.task28.task2817;
+
+import java.time.LocalTime;
+import java.util.Date;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+/* 
+Полусекундомер
+*/
+
+public class Solution {
+
+    public static ScheduledExecutorService scheduledPool = Executors.newScheduledThreadPool(3);
+
+    public static void main(String[] args) throws Exception {
+        Runnable runnable = () -> System.out.println(LocalTime.now());
+        scheduledPool.scheduleAtFixedRate(runnable,0L,500L, TimeUnit.MILLISECONDS);
+
+        Thread.sleep(3000);
+        scheduledPool.shutdown();
+    }
+}
