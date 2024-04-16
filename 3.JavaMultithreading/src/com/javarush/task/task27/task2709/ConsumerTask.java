0dcbb11a71f2234503ee39c1fddac4fb5197dba1
HeVetal <heavyve@gmail.com>
2024-04-16 15:05:18 +0300

    add solution JavaMultithreading task 2709

---
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2709/ConsumerTask.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task27.task2709;
+
+public class ConsumerTask implements Runnable {
+    private TransferObject transferObject;
+    protected volatile boolean stopped;
+
+    public ConsumerTask(TransferObject transferObject) {
+        this.transferObject = transferObject;
+        new Thread(this, "ConsumerTask").start();
+    }
+
+    public void run() {
+        synchronized (transferObject) {
+            while (!stopped) {
+                transferObject.get();
+            }
+        }
+    }
+
+    public void stop() {
+        stopped = true;
+    }
+}
