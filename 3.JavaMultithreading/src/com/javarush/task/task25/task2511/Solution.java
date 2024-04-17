54d27493f0c0e896629ce4fd01442826f0a9b824
HeVetal <heavyve@gmail.com>
2024-04-17 16:00:49 +0300

    add solution JavaMultithreading task 2511

---
+++ 3.JavaMultithreading/src/com/javarush/task/task25/task2511/Solution.java
@@ -0,0 +1,54 @@
+package com.javarush.task.task25.task2511;
+
+import java.util.TimerTask;
+
+/* 
+Вооружаемся до зубов!
+*/
+
+public class Solution extends TimerTask {
+    protected TimerTask original;
+    protected final Thread.UncaughtExceptionHandler handler;
+
+    public Solution(TimerTask original) {
+        if (original == null) {
+            throw new NullPointerException();
+        }
+        this.original = original;
+        this.handler = new Thread.UncaughtExceptionHandler() {
+
+            @Override
+            public void uncaughtException(Thread t, Throwable e) {
+                System.out.println(e.getMessage().replace(t.getName(), t.getName().replaceAll(".", "*")));
+            }
+        };    //init handler here
+    }
+
+    public void run() {
+        try {
+            original.run();
+        } catch (Throwable cause) {
+            Thread currentThread = Thread.currentThread();
+            handler.uncaughtException(currentThread, new Exception("Blah " + currentThread.getName() + " blah-blah-blah", cause));
+        }
+    }
+
+    public long scheduledExecutionTime() {
+        return original.scheduledExecutionTime();
+    }
+
+    public boolean cancel() {
+        return original.cancel();
+    }
+
+    public static void main(String[] args) {
+//        Solution solution = new Solution(new TimerTask() {
+//            @Override
+//            public void run() {
+//                throw new RuntimeException();
+//            }
+//        });
+//
+//        solution.run();
+    }
+}
