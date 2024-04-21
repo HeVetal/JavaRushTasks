edb29ae8d4fdfdf0ea5d46a9d3bb2eda0b892188
HeVetal <heavyve@gmail.com>
2024-04-21 11:17:20 +0300

    add solution JavaMultithreading task 3110/2 not finish(createZip)

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ZipFileManager.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ZipFileManager.java
@@ -1,6 +1,9 @@
 package com.javarush.task.task31.task3110;
 
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 public class ZipFileManager {
     private Path zipFile;
@@ -10,5 +13,8 @@ public class ZipFileManager {
     }
 
     public void createZip(Path source) throws Exception {
+        ZipOutputStream zip = (ZipOutputStream) Files.newOutputStream(zipFile);
+        ZipEntry zipEntry = new ZipEntry(String.valueOf(source.getFileName()));
+        zip.write(zipEntry.getMethod());
     }
 }
