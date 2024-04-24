b45173c41b72a098526eeb886538e321fb10436a
HeVetal <heavyve@gmail.com>
2024-04-24 17:35:25 +0300

    add solution JavaMultithreading task 3209/2

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
@@ -15,6 +15,14 @@ public class View extends JFrame implements ActionListener {
         this.controller = controller;
     }
 
+    public void init() {
+
+    }
+
+    public void exit(){
+        controller.exit();
+    }
+
     @Override
     public void actionPerformed(ActionEvent e) {
 
