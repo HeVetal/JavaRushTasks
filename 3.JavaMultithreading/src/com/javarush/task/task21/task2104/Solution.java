384f7888909e7ff21f568b50d2a10a45dc3ebb75
Vetal <heavyve@gmail.com>
2024-04-26 23:32:03 +0300

    add solution JavaMultithreading task 2104 not finish

---
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2104/Solution.java
@@ -0,0 +1,39 @@
+package com.javarush.task.task21.task2104;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+/* 
+Equals and HashCode
+*/
+
+public class Solution {
+    private final String first, last;
+
+    public Solution(String first, String last) {
+        this.first = first;
+        this.last = last;
+    }
+
+    public boolean equals(Object n) {
+        if (this == n) return true;
+        if (!(n instanceof Solution)) return false;
+        Solution that = (Solution) n;
+        if (this.first == null) return false;
+        if (this.last == null) return false;
+        return this.first.equals(that.first) && this.last.equals(that.last);
+    }
+
+    public int hashCode() {
+        int result = first != null ? first.hashCode() : 0;
+        result = 31 * result + (last != null ? last.hashCode() : 0);
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Set<Solution> s = new HashSet<>();
+        s.add(new Solution("Donald", "Duck"));
+        System.out.println(s.contains(new Solution("Donald", "Duck")));
+    }
+}
