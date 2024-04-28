dc511b574dbf4f32e5120811a4483098ee23de3b
Vetal <heavyve@gmail.com>
2024-04-28 09:07:29 +0300

    add solution JavaMultithreading task 2711

---
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2711/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task27.task2711;
+
+import java.util.concurrent.CountDownLatch;
+
+/* 
+CountDownLatch
+*/
+
+public class Solution {
+
+    CountDownLatch latch = new CountDownLatch(1);
+
+    public void someMethod() throws InterruptedException {
+        latch.await();
+        latch.countDown();
+        retrieveValue();
+
+    }
+
+    void retrieveValue() {
+        System.out.println("Value retrieved.");
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
