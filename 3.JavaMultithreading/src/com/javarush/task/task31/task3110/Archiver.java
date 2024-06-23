ed5026829ec63985a9122290fc6f9a85664d05c8
Vetal <heavyve@gmail.com>
2024-06-23 23:46:53 +0300

    add solution JavaMultithreading task 3110/4

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
@@ -1,11 +1,13 @@
 package com.javarush.task.task31.task3110;
 
+import com.javarush.task.task31.task3110.command.ExitCommand;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.nio.file.Paths;
 
 public class Archiver {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         try {
             BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
@@ -18,6 +20,7 @@ public class Archiver {
             throw new RuntimeException(e);
         }
 
-
+        ExitCommand exitCommand = new ExitCommand();
+        exitCommand.execute();
     }
 }
