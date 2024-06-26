7c553077dbde0aa330ea7371280597dc600a5922
Vetal <heavyve@gmail.com>
2024-06-26 22:43:13 +0300

    add solution JavaMultithreading task 3110/6

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/CommandExecutor.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/CommandExecutor.java
@@ -1,12 +1,26 @@
 package com.javarush.task.task31.task3110;
 
-import com.javarush.task.task31.task3110.command.Command;
+import com.javarush.task.task31.task3110.command.*;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class CommandExecutor {
     private CommandExecutor() {
     }
 
-    private static Map<Operation, Command> ALL_KNOWN_COMMANDS_MAP;
+    private static final Map<Operation, Command> ALL_KNOWN_COMMANDS_MAP = new HashMap<>();
+
+    static {
+        ALL_KNOWN_COMMANDS_MAP.put(Operation.ADD, new ZipAddCommand());
+        ALL_KNOWN_COMMANDS_MAP.put(Operation.CREATE, new ZipCreateCommand());
+        ALL_KNOWN_COMMANDS_MAP.put(Operation.EXIT, new ExitCommand());
+        ALL_KNOWN_COMMANDS_MAP.put(Operation.CONTENT, new ZipContentCommand());
+        ALL_KNOWN_COMMANDS_MAP.put(Operation.EXTRACT, new ZipExtractCommand());
+        ALL_KNOWN_COMMANDS_MAP.put(Operation.REMOVE, new ZipRemoveCommand());
+    }
+
+    public static void execute(Operation operation) throws Exception{
+        ALL_KNOWN_COMMANDS_MAP.get(operation).execute();
+    }
 }
