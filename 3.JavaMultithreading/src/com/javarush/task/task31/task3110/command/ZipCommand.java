e33b222e543ac54cc4956f6b812775d4ab3df39a
Vetal <heavyve@gmail.com>
2024-07-08 22:28:02 +0300

    add solution JavaMultithreading task 3110/12 not finish add FileProperties

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipCommand.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipCommand.java
@@ -3,16 +3,14 @@ package com.javarush.task.task31.task3110.command;
 import com.javarush.task.task31.task3110.ConsoleHelper;
 import com.javarush.task.task31.task3110.ZipFileManager;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public abstract class ZipCommand implements Command {
 
     public ZipFileManager getZipFileManager() throws Exception {
-        ConsoleHelper.writeMessage("Введите полный путь архива");
-        Path path = Paths.get(ConsoleHelper.readString());
-        return new ZipFileManager(path);
+        ConsoleHelper.writeMessage("Введите полный путь файла архива:");
+        Path zipPath = Paths.get(ConsoleHelper.readString());
+        return new ZipFileManager(zipPath);
     }
 }
