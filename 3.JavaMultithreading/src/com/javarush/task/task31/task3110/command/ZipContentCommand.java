bc85a280755a9957d8be1a6aa29778a55337a7f5
HeVetal <heavyve@gmail.com>
2024-07-16 16:21:38 +0300

    add solution JavaMultithreading task 3110/14

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipContentCommand.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipContentCommand.java
@@ -1,8 +1,21 @@
 package com.javarush.task.task31.task3110.command;
 
-public class ZipContentCommand extends ZipCommand{
+import com.javarush.task.task31.task3110.ConsoleHelper;
+import com.javarush.task.task31.task3110.FileProperties;
+import com.javarush.task.task31.task3110.ZipFileManager;
+
+import java.util.List;
+
+public class ZipContentCommand extends ZipCommand {
     @Override
     public void execute() throws Exception {
-
+        ConsoleHelper.writeMessage("Просмотр содержимого архива.");
+        ZipFileManager zipFileManager = getZipFileManager();
+        ConsoleHelper.writeMessage("Содержимое архива:");
+        List<FileProperties> filesList = zipFileManager.getFilesList();
+        for (FileProperties fileProperties : filesList) {
+            ConsoleHelper.writeMessage(fileProperties.toString());
+        }
+        ConsoleHelper.writeMessage("Содержимое архива прочитано.");
     }
 }
