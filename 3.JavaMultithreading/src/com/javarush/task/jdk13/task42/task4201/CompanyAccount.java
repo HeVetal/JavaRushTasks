8e723cc70bc676afb173a425ee00e579fc6909e9
Vetal <heavyve@gmail.com>
2024-08-19 23:27:26 +0300

    add solution Multithreading task 4101

---
+++ 3.JavaMultithreading/src/com/javarush/task/jdk13/task42/task4201/CompanyAccount.java
@@ -0,0 +1,22 @@
+package com.javarush.task.jdk13.task42.task4201;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class CompanyAccount {
+    private final AtomicInteger totalMoney;
+
+    public CompanyAccount(int totalMoney) {
+
+        this.totalMoney = new AtomicInteger(totalMoney);
+    }
+
+    public int getTotalMoney() {
+
+        return totalMoney.get();
+    }
+
+    public void addMoney(int money) {
+
+        totalMoney.addAndGet(money);
+    }
+}
