ef7b4c39204478f6975ece7adc93668517381473
HeVetal <heavyve@gmail.com>
2024-08-26 09:50:59 +0300

    add solution Collection task2213

--- 4.JavaCollections/src/com/javarush/task/task22/task2213/Tetris.java
+++ 4.JavaCollections/src/com/javarush/task/task22/task2213/Tetris.java
@@ -3,6 +3,7 @@ package com.javarush.task.task22.task2213;
 public class Tetris {
     private Field field;
     private Figure figure;
+    static Tetris game;
 
     public Field getField() {
         return field;
@@ -17,6 +18,8 @@ public class Tetris {
     public void step(){}
 
     public static void main(String[] args) {
-
+        Tetris tetris = new Tetris();
+        game = tetris;
+        game.run();
     }
 }
