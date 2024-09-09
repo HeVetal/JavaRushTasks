ce0ab4cbf924543abd15087ab23202e3a957f065
Vetal <heavyve@gmail.com>
2024-09-09 23:53:38 +0300

    add solution Collection task2213 / 14 not finish

--- 4.JavaCollections/src/com/javarush/task/task22/task2213/Field.java
+++ 4.JavaCollections/src/com/javarush/task/task22/task2213/Field.java
@@ -1,8 +1,16 @@
 package com.javarush.task.task22.task2213;
 
+import java.util.ArrayList;
+
+/**
+ * Класс Field описывает "поле клеток" игры Тетрис
+ */
 public class Field {
+    //ширина и высота
     private int width;
     private int height;
+
+    //матрица поля: 1 - клетка занята, 0 - свободна
     private int[][] matrix;
 
     public Field(int width, int height) {
@@ -23,26 +31,84 @@ public class Field {
         return matrix;
     }
 
-    public void print() {
-        for (int i = 0; i < height; i++) {
-            for (int j = 0; j < width; j++) {
-                if (matrix[i][j] == 0)
-                    System.out.print(".");
-                else
-                    System.out.print("X");
-            }
-            System.out.println();
-        }
-    }
-
-    public void removeFullLines() {
-    }
-
+    /**
+     * Метод возвращает значение, которое содержится в матрице с координатами (x,y)
+     * Если координаты за пределами матрицы, метод возвращает null.
+     */
     public Integer getValue(int x, int y) {
+        if (x >= 0 && x < width && y >= 0 && y < height)
+            return matrix[y][x];
+
         return null;
     }
 
-    void setValue(int x, int y, int value) {
+    /**
+     * Метод устанавливает переданное значение(value) в ячейку матрицы с координатами (x,y)
+     */
+    public void setValue(int x, int y, int value) {
+        if (x >= 0 && x < width && y >= 0 && y < height)
+            matrix[y][x] = value;
+    }
 
+    /**
+     * Метод печатает на экран текущее содержание матрицы
+     */
+    public void print() {
+        //Создаем массив, куда будем "рисовать" текущее состояние игры
+        int[][] canvas = new int[height][width];
+
+        //Копируем "матрицу поля" в массив
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                canvas[i][j] = matrix[i][j];
+            }
+        }
+
+        //Копируем фигурку в массив, только непустые клетки
+        int left = Tetris.game.getFigure().getX();
+        int top = Tetris.game.getFigure().getY();
+        int[][] brickMatrix = Tetris.game.getFigure().getMatrix();
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (top + i >= height || left + j >= width) continue;
+                if (brickMatrix[i][j] == 1)
+                    canvas[top + i][left + j] = 2;
+            }
+        }
+
+
+        //Выводим "нарисованное" на экран, но начинаем с "границы кадра".
+        System.out.println("---------------------------------------------------------------------------\n");
+
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                int index = canvas[i][j];
+                if (index == 0)
+                    System.out.print(" . ");
+                else if (index == 1)
+                    System.out.print(" X ");
+                else if (index == 2)
+                    System.out.print(" X ");
+                else
+                    System.out.print("???");
+            }
+            System.out.println();
+        }
+
+
+        System.out.println();
+        System.out.println();
+    }
+
+    /**
+     * Удаляем заполненные линии
+     */
+    public void removeFullLines() {
+        //Например так:
+        //Создаем список для хранения линий
+        //Копируем все неполные линии в список.
+        //Добавляем недостающие строки в начало списка.
+        //Преобразуем список обратно в матрицу
     }
 }
