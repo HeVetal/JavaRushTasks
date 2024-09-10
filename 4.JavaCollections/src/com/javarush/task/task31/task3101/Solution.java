1f47bdf122c8c07373cc72155c04f30547378a20
HeVetal <heavyve@gmail.com>
2024-09-10 11:46:51 +0300

    add solution Collection task3101

--- 4.JavaCollections/src/com/javarush/task/task31/task3101/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task31/task3101/Solution.java
@@ -1,32 +1,49 @@
 package com.javarush.task.task31.task3101;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
-import java.util.EnumSet;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.*;
 
 /* 
 Проход по дереву файлов
 */
 
 public class Solution {
-    public static void main(String[] args) {
-//        Path path = Path.of(args[0]);
-//        Path resultFileAbsolutePath = Paths.get(args[1]);
-//        FileUtils.renameFile(resultFileAbsolutePath, Paths.get(allFilesContent.txt));
-        String path = args[0];
-        String resultFileAbsolutePath = args[1];
-        try {
-            File resultFile = new File(resultFileAbsolutePath);
-            File dest = new File(resultFile.getParentFile() + "/allFilesContent.txt ");
-            FileUtils.renameFile(resultFile, dest);
+    public static void main(String[] args) throws IOException {
+        File sourceDirectory = new File(args[0]);
+        File resultFile = new File(args[1]);
+        File allFilesContent = new File(resultFile.getParent() + "/allFilesContent.txt");
+        if (FileUtils.isExist(allFilesContent)) {
+            FileUtils.deleteFile(allFilesContent);
+        }
+        FileUtils.renameFile(resultFile, allFilesContent);
+        List<File> list = new ArrayList<>();
+        getFiles(sourceDirectory, list);
+        try (FileOutputStream outputStream = new FileOutputStream(allFilesContent, true)) {
+            for (File file : list) {
+                if (file.length() <= 50) {
+                    outputStream.write(Files.readAllBytes(file.toPath()));
+                    outputStream.write("\n".getBytes());
+                }
+            }
+        }
+    }
 
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+    private static void getFiles(File sourceDirectory, List<File> list) {
+        File[] files = sourceDirectory.listFiles();
+        if (Objects.isNull(files)) {
+            return;
+        }
+        for (File file : files) {
+            if (file.isDirectory()) {
+                getFiles(file, list);
+            } else {
+                list.add(file);
+            }
         }
     }
 }
