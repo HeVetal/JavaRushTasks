d63f352daece253c3cf5223f9399d3e9ec56d25e
Vetal <heavyve@gmail.com>
2024-09-28 23:35:58 +0300

    add solution Collection task2213 / 15 not finish

--- 4.JavaCollections/src/com/javarush/task/task22/task2213/Field.java
+++ 4.JavaCollections/src/com/javarush/task/task22/task2213/Field.java
@@ -105,18 +105,28 @@ public class Field {
      * Удаляем заполненные линии
      */
     public void removeFullLines() {
-        //Например так:
         //Создаем список для хранения линий
-        ArrayList<int[]> lines = new ArrayList<>();
+        ArrayList<int[]> lines = new ArrayList<int[]>();
 
         //Копируем все неполные линии в список.
         for (int i = 0; i < height; i++) {
+            //подсчитываем количество единиц в строке - просто суммируем все ее значения
             int count = 0;
             for (int j = 0; j < width; j++) {
                 count += matrix[i][j];
             }
+
+            //Если сумма строки не равна ее ширине - добавляем в список
+            if (count != width)
+                lines.add(matrix[i]);
         }
+
         //Добавляем недостающие строки в начало списка.
+        while (lines.size() < height) {
+            lines.add(0, new int[width]);
+        }
+
         //Преобразуем список обратно в матрицу
+        matrix = lines.toArray(new int[height][width]);
     }
 }
