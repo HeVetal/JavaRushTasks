9d2d1f531e8435c27e1a25feb115604eefd06a7a
Vetal <heavyve@gmail.com>
2024-10-12 21:42:35 +0300

    add solution Collection task2213 / 16 not finish

--- 4.JavaCollections/src/com/javarush/task/task22/task2213/Figure.java
+++ 4.JavaCollections/src/com/javarush/task/task22/task2213/Figure.java
@@ -47,6 +47,11 @@ public class Figure {
 
     public void rotate(){
         int[][] matrix2 = new int[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                matrix2[i][j] = matrix[j][i];
+            }
+        }
     }
 
     public void downMaximum(){}
