07dae8f2d622b0e1a135ae2008e77d2c1e672f73
HeVetal <heavyve@gmail.com>
2024-10-22 16:30:20 +0300

    add solution Collection task3312

---
+++ 4.JavaCollections/src/com/javarush/task/task33/task3312/Zoo.java
@@ -0,0 +1,46 @@
+package com.javarush.task.task33.task3312;
+
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.JsonTypeName;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Zoo {
+    public List<Animal> animals = new ArrayList<>();
+
+    @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type")
+    @JsonSubTypes({
+            @JsonSubTypes.Type(value = Dog.class, name = "dog"),
+            @JsonSubTypes.Type(value = Cat.class, name = "cat")
+    })
+    public static class Animal {
+        public Animal(String name) {
+            this.name = name;
+        }
+
+        public String name;
+    }
+
+    @JsonTypeName(value = "dog")
+    public static class Dog extends Animal {
+
+        public double barkVolume;
+
+        public Dog(String name) {
+            super(name);
+        }
+    }
+
+    @JsonTypeName(value = "cat")
+    public static class Cat extends Animal {
+        boolean likesCream;
+        public int lives;
+
+        public Cat(String name) {
+            super(name);
+        }
+    }
+}
