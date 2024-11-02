61a1e5ea20210c7f92e8a9e3bcaff1446c7fa232
HeVetal <heavyve@gmail.com>
2024-11-02 15:18:15 +0300

    solution Multithreading task4307 refactor to stream

--- 3.JavaMultithreading/src/com/javarush/task/jdk13/task43/task4307/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/jdk13/task43/task4307/Solution.java
@@ -2,6 +2,7 @@ package com.javarush.task.jdk13.task43.task4307;
 
 import org.apache.commons.lang3.ObjectUtils;
 
+import java.util.Arrays;
 import java.util.Objects;
 
 /* 
@@ -19,23 +20,27 @@ public class Solution {
     }
 
     public static void printFirstNonNull(final String... values) {
-        if(!Objects.isNull(values)){
-            for (String value : values) {
-                if(Objects.nonNull(value)){
-                    System.out.println(value);
-                    break;
-                }
-            }
-        }
-        //System.out.println(ObjectUtils.firstNonNull(values));
+//        if(!Objects.isNull(values)){
+//            for (String value : values) {
+//                if(Objects.nonNull(value)){
+//                    System.out.println(value);
+//                    break;
+//                }
+//            }
+//        }
+        System.out.println(Arrays.stream(values).filter(Objects::nonNull).findFirst().get());
     }
 
     public static void printDefaultValueIfNullObject(final String[] values, final String defaultValue) {
 //        for (String o : values) {
 //            System.out.println(ObjectUtils.defaultIfNull(o, defaultValue));
 //        }
-        for (String value : values) {
-            System.out.println(Objects.requireNonNullElse(value, defaultValue));
-        }
+//        for (String value : values) {
+//            System.out.println(Objects.requireNonNullElse(value, defaultValue));
+////        }
+
+//        Arrays.asList(values).replaceAll(s -> Objects.isNull(s) ? defaultValue : s);
+//        Arrays.stream(values).forEach(System.out::println);
+        Arrays.stream(values).map(s -> s != null ? s : defaultValue).forEach(System.out::println);
     }
 }
