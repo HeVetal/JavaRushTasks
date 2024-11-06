202c0f098c5151595d499d9b3225631176e57090
HeVetal <heavyve@gmail.com>
2024-11-06 15:34:06 +0300

    add solution Collection task2515

--- 4.JavaCollections/src/com/javarush/task/task25/task2515/Space.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/Space.java
@@ -1,7 +1,53 @@
 package com.javarush.task.task25.task2515;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Space {
+    private int width;
+    private int height;
+    private SpaceShip ship;
+    private List<Ufo> ufos;
+    private List<Rocket> rockets;
+    private List<Bomb> bombs;
+
+    public Space(int width, int height) {
+        this.width = width;
+        this.height = height;
+        ufos = new ArrayList<>();
+        rockets = new ArrayList<>();
+        bombs = new ArrayList<>();
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public SpaceShip getShip() {
+        return ship;
+    }
+
+    public List<Ufo> getUfos() {
+        return ufos;
+    }
+
+    public List<Rocket> getRockets() {
+        return rockets;
+    }
+
+    public List<Bomb> getBombs() {
+        return bombs;
+    }
+
+    public void setShip(SpaceShip ship) {
+        this.ship = ship;
+    }
+
     public static void main(String[] args) {
-        
+
     }
 }
