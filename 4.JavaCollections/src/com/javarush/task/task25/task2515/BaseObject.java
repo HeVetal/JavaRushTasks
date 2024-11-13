713183f16653696f860c1be819265077e844aa3c
HeVetal <heavyve@gmail.com>
2024-11-13 16:36:52 +0300

    add solution Collection task2515/5

--- 4.JavaCollections/src/com/javarush/task/task25/task2515/BaseObject.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/BaseObject.java
@@ -4,6 +4,40 @@ public abstract class BaseObject {
     private double x;
     private double y;
     private double radius;
+    private boolean isAlive;
 
+    public BaseObject(double x, double y, double radius) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        isAlive = true;
+    }
 
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public boolean isAlive() {
+        return isAlive;
+    }
 }
--- 4.JavaCollections/src/com/javarush/task/task25/task2515/Ufo.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/Ufo.java
@@ -1,4 +1,7 @@
 package com.javarush.task.task25.task2515;
 
-public class Ufo extends BaseObject{
+public class Ufo extends BaseObject {
+    public Ufo(double x, double y, double radius) {
+        super(x, y, radius);
+    }
 }
