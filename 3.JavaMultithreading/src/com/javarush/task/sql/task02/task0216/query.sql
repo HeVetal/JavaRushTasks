ef6eb42a985fae896848c9840206c52639023841
HeVetal <heavyve@gmail.com>
2024-12-02 15:51:02 +0300

    add solution JavaMultithreading sql task 0216

---
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task02/task0216/query.sql
@@ -0,0 +1,8 @@
+SELECT
+    CASE
+           WHEN position = 'backend developer' THEN 'yes'
+           WHEN position = 'frontend developer' THEN 'yes'
+           ELSE 'no'
+    END
+FROM employee
+WHERE department = 'cool devs'
