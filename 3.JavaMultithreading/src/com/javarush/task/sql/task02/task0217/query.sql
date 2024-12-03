669584703c98bb99a8760e95e325c829c4e0870a
HeVetal <heavyve@gmail.com>
2024-12-03 16:34:48 +0300

    add solution JavaMultithreading sql task 0217

---
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task02/task0217/query.sql
@@ -0,0 +1,9 @@
+SELECT CASE
+           WHEN euro > 5 THEN 'best'
+           WHEN euro = 5 THEN 'good'
+           ELSE 'bad'
+           END
+FROM cars
+WHERE prod_year > 2020
+ORDER BY price ASC
+LIMIT 5
