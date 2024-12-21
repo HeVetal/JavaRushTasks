6bbb51133b1ba827f5a63db07b0b72e678de1c32
Vetal <heavyve@gmail.com>
2024-12-21 23:32:10 +0300

    add solution JavaCollections task 2515/9 not finish

--- 4.JavaCollections/src/com/javarush/task/task25/task2515/Canvas.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/Canvas.java
@@ -41,4 +41,14 @@ public class Canvas {
         }
     }
 
+    public void clear(){
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                matrix[i][j] = ' ';
+            }
+        }
+    }
+
+
+
 }
