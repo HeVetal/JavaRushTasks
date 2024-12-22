c995cee2e1d69380ff9e1fb6f7feefad248d23dc
Vetal <heavyve@gmail.com>
2024-12-22 23:19:54 +0300

    add solution JavaCollections task 2515/9

--- 4.JavaCollections/src/com/javarush/task/task25/task2515/Canvas.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/Canvas.java
@@ -41,12 +41,26 @@ public class Canvas {
         }
     }
 
-    public void clear(){
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
-                matrix[i][j] = ' ';
+    public void clear() {
+        this.matrix = new char[height][width];
+    }
+
+    public void print() {
+        System.out.println();
+
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                System.out.print(" ");
+                System.out.print(matrix[i][j]);
+                System.out.print(" ");
             }
+
+            System.out.println();
         }
+
+        System.out.println();
+        System.out.println();
+        System.out.println();
     }
 
 
