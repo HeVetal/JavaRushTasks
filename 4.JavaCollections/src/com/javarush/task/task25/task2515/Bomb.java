0de4d46b0f294be84f2b9c16d422a82ca0c1c5f9
Vetal <heavyve@gmail.com>
2024-12-24 19:35:14 +0300

    add solution JavaCollections task 2515/10 not finished

--- 4.JavaCollections/src/com/javarush/task/task25/task2515/Bomb.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/Bomb.java
@@ -1,7 +1,11 @@
 package com.javarush.task.task25.task2515;
 
 public class Bomb extends BaseObject{
-    public Bomb(double x, double y, double radius) {
-        super(x, y, radius);
+    public Bomb(double x, double y) {
+        super(x, y, 1);
+    }
+
+    public void move(){
+        y++;
     }
 }
--- 4.JavaCollections/src/com/javarush/task/task25/task2515/Canvas.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/Canvas.java
@@ -1,5 +1,6 @@
 package com.javarush.task.task25.task2515;
 
+
 public class Canvas {
     private int width;
     private int height;
@@ -8,27 +9,11 @@ public class Canvas {
     public Canvas(int width, int height) {
         this.width = width;
         this.height = height;
-        matrix = new char[height][width];
+        this.matrix = new char[height + 2][width + 2];
     }
 
-    public int getWidth() {
-        return width;
-    }
-
-    public int getHeight() {
-        return height;
-    }
-
-    public char[][] getMatrix() {
-        return matrix;
-    }
-
-    public void setPoint(double x, double y, char c) {
-        int roundX = (int) Math.round(x);
-        int roundY = (int) Math.round(y);
-        if (roundX >= 0 && roundX < matrix[0].length && roundY >= 0 && roundY < matrix.length) {
-            matrix[roundY][roundX] = c;
-        }
+    public void clear() {
+        this.matrix = new char[height + 2][width + 2];
     }
 
     public void drawMatrix(double x, double y, int[][] matrix, char c) {
@@ -41,15 +26,19 @@ public class Canvas {
         }
     }
 
-    public void clear() {
-        this.matrix = new char[height][width];
+    public void setPoint(double x, double y, char c) {
+        int xRounded = (int) Math.round(x);
+        int yRounded = (int) Math.round(y);
+        if (xRounded >= 0 && xRounded < matrix[0].length && yRounded >= 0 && yRounded < matrix.length) {
+            matrix[yRounded][xRounded] = c;
+        }
     }
 
     public void print() {
         System.out.println();
 
-        for (int i = 0; i < height; i++) {
-            for (int j = 0; j < width; j++) {
+        for (int i = 0; i < height + 2; i++) {
+            for (int j = 0; j < width + 2; j++) {
                 System.out.print(" ");
                 System.out.print(matrix[i][j]);
                 System.out.print(" ");
@@ -63,6 +52,15 @@ public class Canvas {
         System.out.println();
     }
 
+    public int getWidth() {
+        return width;
+    }
 
+    public int getHeight() {
+        return height;
+    }
 
+    public char[][] getMatrix() {
+        return matrix;
+    }
 }
