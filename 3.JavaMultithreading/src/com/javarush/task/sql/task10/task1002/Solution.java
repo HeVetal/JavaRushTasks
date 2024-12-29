3af59b7de2f86118c9cbbd215fafb34de30da726
HeVetal <heavyve@gmail.com>
2024-12-29 19:33:36 +0300

    add solution JavaMultithreading sql task 1002 not verified

---
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task10/task1002/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.sql.task10.task1002;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+
+/* 
+task1002
+*/
+
+public class Solution {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println(getDirector());
+    }
+
+    public static Employee getDirector() {
+        try (SessionFactory sessionFactory = MySessionFactory.getSessionFactory()) {
+            Session session = sessionFactory.openSession();
+            String hql ="from Employee where smth = 'director'";
+            Query<Employee> query = session.createQuery(hql);
+            return query.uniqueResult();
+        }
+    }
+}
