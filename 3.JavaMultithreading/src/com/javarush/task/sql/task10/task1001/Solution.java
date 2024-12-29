8f7a47d98b326b9ae2a88e7a23aca0a72bbb49a3
HeVetal <heavyve@gmail.com>
2024-12-29 19:33:26 +0300

    add solution JavaMultithreading sql task 1001 not verified

---
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task10/task1001/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.sql.task10.task1001;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+
+/* 
+task1001
+*/
+
+public class Solution {
+
+    public static void main(String[] args) throws Exception {
+        try (SessionFactory sessionFactory = MySessionFactory.getSessionFactory()) {
+            String hql = "select distinct smth from Employee where age > 18 order by smth";
+            Session session = sessionFactory.openSession();
+            Query<String> query = session.createQuery(hql, String.class);
+            query.stream().forEach(System.out::println);
+        }
+    }
+}
