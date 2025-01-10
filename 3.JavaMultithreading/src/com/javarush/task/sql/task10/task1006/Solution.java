48a6a3d8dce0d7185fc694cd7b9fe4cccda5e276
Vetal <heavyve@gmail.com>
2025-01-10 09:29:12 +0300

    add sql task 1006

---
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task10/task1006/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.task.sql.task10.task1006;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+/* 
+task1006
+*/
+
+public class Solution {
+
+    public static void main(String[] args) throws Exception {
+        getBetween(25, 32).forEach(System.out::println);
+    }
+
+    public static List<Employee> getBetween(int from, int to) {
+        try (SessionFactory sessionFactory = MySessionFactory.getSessionFactory()) {
+            Session session = sessionFactory.openSession();
+            String hql = "from Employee where age > :from and age < :to order by age asc";
+            Query<Employee> query = session.createQuery(hql, Employee.class);
+            query.setParameter("from", from);
+            query.setParameter("to", to);
+
+            return query.list();
+        }
+    }
+}
