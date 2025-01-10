7171254acfc189c32b1c36b2379dec7794d27c3f
Vetal <heavyve@gmail.com>
2025-01-10 09:29:01 +0300

    add sql task 1004

---
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task10/task1004/Publisher.java
@@ -0,0 +1,31 @@
+package com.javarush.task.sql.task10.task1004;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "publisher")
+public class Publisher {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
