1c8b6f11ff7f89c327a6e7a0cf0a30571bed65f7
HeVetal <heavyve@gmail.com>
2025-01-13 15:01:01 +0300

    add solution JavaMultithreading sql task 0334

---
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0334/query.sql
@@ -0,0 +1,9 @@
+SELECT author.last_name AS author,
+       author.country   AS author_country,
+       book.genre     AS book_genre
+FROM authors AS author LEFT JOIN books AS book
+      ON author.author_id = book.author_id
+WHERE book.genre LIKE 'fantasy'
+GROUP BY author.last_name, author.country, book.genre
+ORDER BY author.country ASC
+LIMIT 5
