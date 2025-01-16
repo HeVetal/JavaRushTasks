ad617bbc2b39fef9c579ef4501be434350618a24
HeVetal <heavyve@gmail.com>
2025-01-16 15:27:23 +0300

    add solution JavaMultithreading sql task 0349

---
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0349/query.sql
@@ -0,0 +1,6 @@
+WITH tempTable(averageDOB) AS (SELECT AVG(year_born)
+                               FROM film_directors)
+SELECT id, full_name, year_born
+FROM film_directors,
+     tempTable
+WHERE film_directors.year_born < tempTable.averageDOB
