f04625ede008790c6852757419df30ee606906c3
HeVetal <heavyve@gmail.com>
2025-01-20 14:43:11 +0300

    add solution JavaMultithreading task 2712/1

---
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/kitchen/Dish.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task27.task2712.kitchen;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public enum Dish {
+    FISH,
+    STEAK,
+    SOUP,
+    JUICE,
+    WATER;
+
+
+    public static String allDishesToString() {
+
+        return Arrays.stream(values()).map(String::valueOf).collect(Collectors.joining(", "));
+    }
+}
