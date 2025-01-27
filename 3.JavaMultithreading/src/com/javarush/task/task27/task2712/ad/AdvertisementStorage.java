1f22ad1f5fe1439ae759479cb1ffe0fe7d21af54
HeVetal <heavyve@gmail.com>
2025-01-27 15:29:38 +0300

    add solution JavaMultithreading 2712 / 6

---
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/AdvertisementStorage.java
@@ -0,0 +1,13 @@
+package com.javarush.task.task27.task2712.ad;
+
+public class AdvertisementStorage {
+    public final static AdvertisementStorage instance = new AdvertisementStorage();
+
+    private AdvertisementStorage() {
+
+    }
+
+    public static AdvertisementStorage getInstance(){
+        return instance;
+    }
+}
