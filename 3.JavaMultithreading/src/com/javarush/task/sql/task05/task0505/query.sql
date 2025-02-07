fcc48ffb151a0ecdae360a649708714aa957a87f
HeVetal <heavyve@gmail.com>
2025-02-07 16:04:22 +0300

    add solution JavaMultithreading SQL task 0505

---
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task05/task0505/query.sql
@@ -0,0 +1,6 @@
+INSERT INTO object (id, name, type, discoverer, discovery_date)
+VALUES (25, '2020 XL5', 'ASTEROID', 'Pan-STARRS 1', '2020-12-12'),
+       (26,'moon','SATELLITE',NULL,NULL),
+       (27,'Jupiter','PLANET',NULL,NULL),
+       (28,'C/1910 A1', 'PLANET', 'Robert T. A.', '1910-01-17')
+
