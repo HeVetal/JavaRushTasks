de5932a056bde57799339812bc9fe05409a1d442
HeVetal <heavyve@gmail.com>
2025-02-14 14:46:49 +0300

    add solution JavaMultithreading task2712 / 11

---
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/statistic/StatisticManager.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task27.task2712.statistic;
+
+import com.javarush.task.task27.task2712.statistic.event.EventDataRow;
+
+public class StatisticManager {
+    private static StatisticManager instance = new StatisticManager();
+
+    private StatisticManager() {
+    }
+
+    public void register(EventDataRow data){
+
+    }
+
+    public static StatisticManager getInstance() {
+        return instance;
+    }
+}
