a20a3656cb9422d3e75d6214751fed58957ab088
HeVetal <heavyve@gmail.com>
2025-02-14 15:09:01 +0300

    add solution JavaMultithreading task2712 / 12

---
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/statistic/event/VideoSelectedEventDataRow.java
@@ -0,0 +1,20 @@
+package com.javarush.task.task27.task2712.statistic.event;
+
+import com.javarush.task.task27.task2712.ad.Advertisement;
+
+import java.util.Date;
+import java.util.List;
+
+public class VideoSelectedEventDataRow implements EventDataRow {
+    private List<Advertisement> optimalVideoSet;
+    private long amount;
+    private int totalDuration;
+    private Date currentDate;
+
+    public VideoSelectedEventDataRow(List<Advertisement> optimalVideoSet, long amount, int totalDuration) {
+        this.optimalVideoSet = optimalVideoSet;
+        this.amount = amount;
+        this.totalDuration = totalDuration;
+        currentDate = new Date();
+    }
+}
