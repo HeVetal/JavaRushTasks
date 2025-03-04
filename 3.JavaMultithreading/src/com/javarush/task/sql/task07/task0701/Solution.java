f0ccc891db07ff2e295e23c15f604ef7198de296
HeVetal <heavyve@gmail.com>
2025-03-04 16:21:00 +0300

    add solution JavaMultithreading SQL task0701

---
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task07/task0701/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.sql.task07.task0701;
+
+import java.sql.*;
+
+/* 
+task0701
+*/
+
+public class Solution {
+
+    public static void main(String[] args) throws Exception {
+        try (Connection connection =
+                     DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "root")) {
+        }
+
+//        Connection connection =
+//                DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "root");
+//        connection.close();
+    }
+}
