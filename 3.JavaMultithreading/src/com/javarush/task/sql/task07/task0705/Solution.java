c81f6a97752e804d663f7efd541ce61cb8976f25
HeVetal <heavyve@gmail.com>
2025-03-06 16:11:17 +0300

    add solution JavaMultithreading SQL task0705

---
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task07/task0705/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.sql.task07.task0705;
+
+import java.sql.*;
+
+/* 
+task0705
+*/
+
+public class Solution {
+
+    public static void main(String[] args) throws Exception {
+        try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/test",
+                "root",
+                "root")) {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT MIN(DISTINCT age) FROM employee");
+
+            while (resultSet.next()) {
+                System.out.println(resultSet.getInt("age"));
+            }
+        }
+    }
+}
