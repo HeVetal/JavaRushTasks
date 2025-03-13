766c86f6eb618b43f71d09975fc772239c3716e9
HeVetal <heavyve@gmail.com>
2025-03-13 17:07:38 +0300

    add solution JavaMultithreading SQL task0804

---
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task08/task0804/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.task.sql.task08.task0804;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+/* 
+task0804
+*/
+
+public class Solution {
+//    batch-режим ?
+
+    public static void main(String[] args) throws Exception {
+        String sql = "insert into employee (name, age, smth) values (?, ?, ?)";
+
+
+        try (Connection connection = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/test", "root", "root");
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+
+            for (int i = 0; i < 5; i++) {
+                preparedStatement.setString(1, "employee_" + i);
+                preparedStatement.setInt(2, 30 + i);
+                preparedStatement.setString(3, "i+" + i);
+
+                preparedStatement.addBatch();
+            }
+            int[] result = preparedStatement.executeBatch();
+        }
+    }
+}
