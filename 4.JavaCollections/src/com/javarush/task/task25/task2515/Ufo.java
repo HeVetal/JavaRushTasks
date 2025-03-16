ae2169f4ab396de4c59c46025f1ca232aeb76fca
Vetal <heavyve@gmail.com>
2025-03-16 23:41:47 +0300

    solution JavaRush JavaCollections task 2515 / 14 not finish

--- 4.JavaCollections/src/com/javarush/task/task25/task2515/Ufo.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/Ufo.java
@@ -4,4 +4,10 @@ public class Ufo extends BaseObject {
     public Ufo(double x, double y, double radius) {
         super(x, y, radius);
     }
+
+    public void draw(Canvas canvas){}
+
+    public void move(){}
+
+    public void fire(){}
 }
