6e07c3920d2da845fd20ef9ad373a1133031a5e7
HeVetal <heavyve@gmail.com>
2025-03-17 16:50:51 +0300

    add solution JavaMultithreading SQL task0903

---
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task09/task0903/Solution.java
@@ -0,0 +1,29 @@
+package com.javarush.task.sql.task09.task0903;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+
+import java.util.Properties;
+
+/*
+task0903
+//*/
+
+public class Solution {
+
+    public static void main(String[] args) throws Exception {
+        Properties properties = new Properties();
+        properties.put(Environment.DRIVER, "com.mysql.jdbc.Driver");
+        properties.put(Environment.URL, "jdbc:mysql://localhost:3306/test");
+        properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQLDialect");
+        properties.put(Environment.USER, "root");
+        properties.put(Environment.PASS, "root");
+
+        Configuration configuration = new Configuration();
+        configuration.setProperties(properties);
+        configuration.addAnnotatedClass(Animal.class);
+
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+    }
+}
